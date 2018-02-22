806b4b453a51010368d204ebd8acf011ca14a2f4
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-02-22 11:57:05 +0800

    Update Yanghui's triangle.cpp

--- template/math/Yanghui's triangle.cpp
+++ template/math/Yanghui's triangle.cpp
@@ -1,20 +1,20 @@
 //求前 n 行
-ll C[maxn][maxn];
+ll C[N][N];
 memset(C, 0, sizeof(C));
-rep(i, 0, maxn) {
+rep(i, 0, N) {
     C[i][0] = C[i][i] = 1;
     rep(j, 1, i)
         C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % MOD;
 }
 
 //求第 n 行（不取模）
-ll C[maxn];
+ll C[N];
 C[0] = 1;
 rep(i, 1, n + 1)
     C[i] = C[i - 1] * (n - i + 1) / i;
 
 //求第 n 行（取模）
-ll C[maxn];
+ll C[N];
 C[0] = 1;
 rep(i, 1, n + 1)
-    C[i] = (C[i - 1] * (n - i + 1) % MOD * inv[i]) % MOD;
+    C[i] = C[i - 1] * (n - i + 1) % MOD * inv[i] % MOD;
