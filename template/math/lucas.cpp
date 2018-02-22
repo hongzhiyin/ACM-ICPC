736a497310034becae4097be6c6cc8651d005d8f
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-02-22 13:35:35 +0800

    Update lucas.cpp

--- template/math/lucas.cpp
+++ template/math/lucas.cpp
@@ -8,7 +8,7 @@ int jc[MOD], ijc[MOD], inv[MOD];
 int C(int a, int b) { return a < b ? 0 : mul(jc[a], mul(ijc[b], ijc[a - b])); }
 int lucas(ll a, ll b) { return ! b ? 1 : mul(C(a % MOD, b % MOD), lucas(a / MOD, b / MOD)); }
 int lucas_ini() {
-	  jc[0] = 1; rep(i, 1, MOD) jc[i] = mul(jc[i - 1], i);    //阶乘
-    inv[1] = 1; rep(i, 2, MOD) inv[i] = mul(inv[MOD % i], MOD - MOD / i);
-    ijc[0] = 1; rep(i, 1, MOD) ijc[i] = mul(ijc[i - 1], inv[i]);
+	jc[0] = 1; rep(i, 1, MOD) jc[i] = mul(jc[i - 1], i);    //阶乘
+	inv[1] = 1; rep(i, 2, MOD) inv[i] = mul(inv[MOD % i], MOD - MOD / i);
+	ijc[0] = 1; rep(i, 1, MOD) ijc[i] = mul(ijc[i - 1], inv[i]);
 }
