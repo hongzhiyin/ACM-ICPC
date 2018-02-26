188f9b2bd14c15145bc8a7527026d07e3e2bfe62
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-02-26 13:28:44 +0800

    Update Union_Find Sets.cpp

--- template/graph/Union_Find Sets.cpp
+++ template/graph/Union_Find Sets.cpp
@@ -10,3 +10,13 @@ void Union(int a,int b) {
     int Fa = Find(a),Fb = Find(b);
     if (Fa != Fb) fa[Fb] = Fa;
 }
+
+void Split(int a, int b)
+{
+    int Fa = Find(a), Fb = b;
+    int tmp = to[a]; to[a] = to[b]; to[b] = tmp;
+    while (Find(b) == Fa) {
+        fa[b] = Fb;
+        b = to[b];
+    }
+}
