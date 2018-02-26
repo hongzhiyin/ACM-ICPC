25aa71b6e2f27b8bf54fe4c77502af85afa7d840
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-02-27 01:21:46 +0800

    Update Prime.cpp

--- template/math/Prime.cpp
+++ template/math/Prime.cpp
@@ -3,9 +3,12 @@ vi prim;
 void GetPrime(int n)
 {
     memset(notPrim, 0, sizeof(notPrim)); prim.clear();
-    rep(i, 2, n+1) if (!notPrim[i]) {
-        prim.pb(i);
-        if (i+i <= n) for (int j = i+i; j <= n; j += i)
-            notPrim[j] = 1;
+    notPrim[0] = notPrim[1] = 1;
+    rep(i, 2, n+1) {
+        if (!notPrim[i]) prim.pb(i);
+        for (int j = 0; j < sz(prim) && i * prim[j] <= n; ++j) {
+            notPrim[i*prim[j]] = 1;
+            if (i % prim[j] == 0) break;
+        }
     }
 }
