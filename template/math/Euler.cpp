05253a2d1eb4b68e862c08a2916e09c6b43e7299
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-02-26 23:33:48 +0800

    Create Euler.cpp

---
+++ template/math/Euler.cpp
@@ -0,0 +1,16 @@
+ll phi(ll n)
+{
+    ll ret = n;
+    for (ll i = 2; i*i <= n; ++i)
+        if (n % i == 0) {
+            ret = ret - ret / i;
+            while (n % i == 0) n /= i;
+         }
+    if (n > 1) ret = ret - ret / n;
+    return ret;
+}
+
+欧拉定理
+A ^ phi(C) ≡ 1 (mod C)
+欧拉降幂公式 ( B > phi(C) )
+A ^ B % C = A ^ (B % phi(C) + phi(C)) % C
