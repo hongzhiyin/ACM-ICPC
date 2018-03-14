1eaceeccfc51dcf9ee767306063e03076b5893e1
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-03-14 11:15:38 +0800

    Update and rename header.cpp to Header.cpp

--- template/base/Header.cpp
+++ template/base/Header.cpp
@@ -32,15 +32,6 @@ const ll LINF = 0x3f3f3f3f3f3f3f3f;
 const int MOD = (int)1e9+7;
 const int N = (int)-1;
 
-//文件输入输出
-#define IO(x) freopen(x".in", "r", stdin); freopen(x".out", "w", stdout);
-
-//浮点型运算
-const db eps = 1e-6;
-bool eq(const db &a, const db &b) { return fabs(a - b) < eps; }
-bool ls(const db &a, const db &b) { return a + eps < b; }
-bool le(const db &a, const db &b) { return eq(a, b) || ls(a, b); }
-
 //常用函数
 char read_char() {char c;for(;;){c=getchar();if(c>='A'&&c<='Z')return c;}}
 ll powmod(ll a,ll b) {ll res=1;a%=MOD;for(;b;b>>=1){if(b&1)res=res*a%MOD;a=a*a%MOD;}return res;}
