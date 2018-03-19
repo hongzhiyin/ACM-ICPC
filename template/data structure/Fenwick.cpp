98b5baa73f802ed5e3f4767b8417ec85e9953c84
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-03-20 00:16:18 +0800

    Update Fenwick.cpp

--- template/data structure/Fenwick.cpp
+++ template/data structure/Fenwick.cpp
@@ -1,5 +1,5 @@
 struct Fenwick {    //注意下标从 1 开始，以及可能需要离散化操作
-    int n, tree[N];
+    int n; ll tree[N];
 
     int Lowbit(int x) { return x & -x; }
 
@@ -9,14 +9,38 @@ struct Fenwick {    //注意下标从 1 开始，以及可能需要离散化操�
     }
 
     int Sum(int x) {
-        int ret = 0;
+        ll ret = 0;
         for (; x > 0; x -= Lowbit(x))
             ret += tree[x];
         return ret;
     }
 
-    void Add(int x, int d) {
+    void Add(int x, ll d) {
         for (; x <= n; x += Lowbit(x))
             tree[x] += d;
     }
 };
+
+//区间修改，区间查询
+Fenwick c1, c2;
+void FenInit(ll *a)     //a[]下标从1开始，a[0] = 0
+{
+    c1.Init(n); c2.Init(n);
+    rep(i, 1, n+1) {
+        c1.Add(i, a[i] - a[i-1]);
+        c2.Add(i, (i-1) * (a[i] - a[i-1]));
+    }
+}
+
+void FenUpdate(ll l, ll r, ll x)
+{
+    c1.Add(l, x); c1.Add(r+1, -x);
+    c2.Add(l, (l-1)*x); c2.Add(r+1, r * -x);
+}
+
+ll FenQuery(ll l, ll r)
+{
+    ll R = 1ll * r * c1.Sum(r) - c2.Sum(r);
+    ll L = 1ll * (l-1) * c1.Sum(l-1) - c2.Sum(l-1);
+    return R - L;
+}
