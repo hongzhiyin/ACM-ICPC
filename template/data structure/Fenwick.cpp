ebabad720d71a48616a7a5a15168a1a6836c4fb0
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-03-19 17:37:53 +0800

    Update Fenwick.cpp

--- template/data structure/Fenwick.cpp
+++ template/data structure/Fenwick.cpp
@@ -1,28 +1,21 @@
 struct Fenwick {    //注意下标从 1 开始，以及可能需要离散化操作
     int n, tree[N];
 
-    int Lowbit(int x) {
-        return x & -x;
-    }
+    int Lowbit(int x) { return x & -x; }
 
     void Init(int n) {
         memset(tree, 0, sizeof(tree));
-        this -> n = n;
+        this->n = n;
     }
 
     int Sum(int x) {
-        int ret = 0;
-        while (x > 0) {
+        for (int ret = 0; x > 0; x -= Lowbit(x))
             ret += tree[x];
-            x -= Lowbit(x);
-        }
         return ret;
     }
 
     void Add(int x, int d) {
-        while (x <= n) {
+        for (; x <= n; x += Lowbit(x))
             tree[x] += d;
-            x += Lowbit(x);
-        }
     }
 };
