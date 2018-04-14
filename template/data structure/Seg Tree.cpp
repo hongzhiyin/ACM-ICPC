a3b0ac708ce801e2f38774f96f138afe1b13a863
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-04-15 02:50:57 +0800

    Create Seg Tree.cpp

---
+++ template/data structure/Seg Tree.cpp
@@ -0,0 +1,56 @@
+#define lson l, m, rt << 1
+#define rson m + 1, r, rt << 1 | 1
+struct SegTree {
+    ll lazy[N << 2], tree[N << 2];
+    
+    void PushUp(int rt) {
+        tree[rt] = tree[rt << 1] + tree[rt << 1 | 1];
+    }
+
+    void PushDown(int rt, int m) {
+        if (lazy[rt]) {
+            lazy[rt << 1] += lazy[rt];
+            lazy[rt << 1 | 1] += lazy[rt];
+            tree[rt << 1] += lazy[rt] * (m - (m >> 1));
+            tree[rt << 1 | 1] += lazy[rt] * (m >> 1);
+            lazy[rt] = 0;
+        }
+    }
+
+    void build(int l, int r, int rt) {
+        lazy[rt] = 0;
+        if (l == r) {
+            scanf("%lld", &tree[rt]);
+            return;
+        }
+        int m = (l + r) >> 1;
+        build(lson);
+        build(rson);
+        PushUp(rt);
+    }
+
+    void update(int L, int R, int c, int l, int r, int rt) {
+        if (L <= l && r <= R) {
+            lazy[rt] += c;
+            tree[rt] += (ll) c * (r - l + 1);
+            return;
+        }
+        PushDown(rt, r - l + 1);
+        int m = (l + r) >> 1;
+        if (L <= m) update(L, R, c, lson);
+        if (m < R) update(L, R, c, rson);
+        PushUp(rt);
+    }
+
+    ll query(int L, int R, int l, int r, int rt) {
+        if (L <= l && r <= R) {
+            return tree[rt];
+        }
+        PushDown(rt, r - l + 1);
+        int m = (l + r) >> 1;
+        ll ret = 0;
+        if (L <= m) ret += query(L, R, lson);
+        if (m < R) ret += query(L, R, rson);
+        return ret;
+    }
+};
