33a2c20aeecdb10218f4522711f59f3c15fb2611
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-04-21 18:44:44 +0800

    Update LIS.cpp

--- template/dynamic programming/LIS.cpp
+++ template/dynamic programming/LIS.cpp
@@ -1,6 +1,6 @@
 rep(i, 1, n+1) g[i] = INF;
 rep(i, 0, n) {
     int k = lower_bound(g+1, g+n+1, a[i]) - g;
-    dp[i] = k;  //dp[i]表示以a[i]结尾的最长子序列的长度
-    g[k] = a[i];    //用a[i]更新g[]中的值使得子序列元素尽可能小
+    dp[i] = k;  //dp[i] 表示以 a[i] 结尾的最长子序列的长度
+    g[k] = a[i];    //用 a[i] 更新 g[] 中的值使得子序列元素尽可能小
 }
