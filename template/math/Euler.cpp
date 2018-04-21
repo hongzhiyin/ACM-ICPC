6bd7643eb65b62a082740239169e03920c1d89ed
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-04-21 20:16:16 +0800

    Update Euler.cpp

--- template/math/Euler.cpp
+++ template/math/Euler.cpp
@@ -10,8 +10,8 @@ ll phi(ll n)
     return ret;
 }
 
-欧拉定理
-A ^ phi(C) ≡ 1 (mod C)
+//欧拉定理
+//A ^ phi(C) ≡ 1 (mod C)
 
-欧拉降幂公式 ( B > phi(C) )
-A ^ B % C = A ^ (B % phi(C) + phi(C)) % C
+//欧拉降幂公式 ( B > phi(C) )
+//A ^ B % C = A ^ (B % phi(C) + phi(C)) % C
