64502e2e595a0f3c0d67bd203a2c91245db448d3
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-04-21 18:49:03 +0800

    Update Trie.cpp

--- template/string/Trie.cpp
+++ template/string/Trie.cpp
@@ -4,18 +4,18 @@ void Init()
     int tot = 0;
 }
 
-void insert()//插入单词s
+void insert()//插入单词 s
 {
-    int len = strlen(s);//单词s的长度
-    int root = 0;//根节点编号为0
+    int len = strlen(s);//单词 s 的长度
+    int root = 0;//根节点编号为 0
     rep(i, 0, len) {
         int id = s[i]-'a';//第二种编号
-        if(!trie[root][id])//如果之前没有从root到id的前缀 
-            trie[root][id] = ++tot;//插入，tot即为第一种编号
+        if(!trie[root][id])//如果之前没有从 root 到 id 的前缀 
+            trie[root][id] = ++tot;//插入， tot 即为第一种编号
         root = trie[root][id];//顺着字典树往下走
         //sum[root]++;//前缀后移一个位置保存
     }
-    /*isw[rt]=true;标志该单词末位字母的尾结点，在查询整个单词时用到*/
+    /*isw[rt]=true; 标志该单词末位字母的尾结点，在查询整个单词时用到*/
 }
 
 bool find()
@@ -27,6 +27,6 @@ bool find()
         root = trie[root][x];//为查询下个字母做准备，往下走 
     }
     return true;//找到了
-    //查询整个单词时，应该return isw[rt] 
-    //return sum[root]; 因为前缀后移了一个保存，所以此时的sum[root]就是要求的前缀出现的次数 
+    //查询整个单词时，应该 return isw[rt] 
+    //return sum[root] ; 因为前缀后移了一个保存，所以此时的 sum[root] 就是要求的前缀出现的次数 
 }
