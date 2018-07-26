67b2dda52ee38a0d59ef8f8e2d3cdf14e1c6d84e
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-27 01:42:53 +0800

    Update Extend.cpp

--- template/base/Extend.cpp
+++ template/base/Extend.cpp
@@ -3,6 +3,13 @@
 
 //浮点型运算
 const db eps = 1e-6;
-bool eq(const db &a, const db &b) { return fabs(a - b) < eps; }
-bool ls(const db &a, const db &b) { return a + eps < b; }
-bool le(const db &a, const db &b) { return eq(a, b) || ls(a, b); }
+bool eq(db a, db b) { return fabs(a - b) < eps; }
+bool ls(db a, db b) { return a + eps < b; }
+bool le(db a, db b) { return eq(a, b) || ls(a, b); }
+
+inline int read() {
+    int k = 0, f = 1; char c = getchar();
+    while (c < '0' || c > '9') c == '-' && (f = -1), c = getchar();
+    while ('0' <= c && c <= '9') k = k * 10 + c - '0', c = getchar();
+    return k * f;
+}
