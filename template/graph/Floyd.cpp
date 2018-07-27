98cdb501f0c3b7bbfe0e83905ec0d4f4a674820e
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-27 18:57:13 +0800

    Update Floyd.cpp

--- template/graph/Floyd.cpp
+++ template/graph/Floyd.cpp
@@ -1,23 +1,18 @@
 struct Floyd {
-    
     int n;
-    int d[maxn][maxn];
-    
+    int d[N][N];
     void Init(int n) {
         this->n = n;
         memset(d,0x3f,sizeof(d));
         rep(i,0,n) d[i][i] = 0;
     }
-
     void AddEdge(int from, int to, int dist) {
         d[from][to] = min(d[from][to], dist);
     }
-    
     void floyd() {
         rep(k,0,n) rep(i,0,n) rep(j,0,n)
             d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
     }
-
     void connect() {
         rep(k,0,n) rep(i,0,n) rep(j,0,n)
             d[i][j] = d[i][j] || (d[i][k] && d[k][j]);
