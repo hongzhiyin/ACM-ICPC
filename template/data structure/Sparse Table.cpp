a21516bba58a7eea09bb955aa42e5cbea89cdfa7
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-30 14:50:18 +0800

    Update Sparse Table.cpp

--- template/data structure/Sparse Table.cpp
+++ template/data structure/Sparse Table.cpp
@@ -1,20 +1,14 @@
 struct ST {
-    int d[maxn][30], p[maxn];
-
-    void Init(int *a, int n) {
+    int d[N][30], p[N];
+    void init(int *a, int n) {
         rep(i, 0, n) d[i][0] = a[i];
         for (int j = 1; (1 << j) <= n; ++j)
             for (int i = 0; i + (1 << j) - 1 < n; ++i)
                 d[i][j] = min(d[i][j - 1], d[i + (1 << (j - 1))][j - 1]);
-        int k = 0;
-        while ((1 << k) <= n) {
-            rep(i, (1 << k), min(n + 1, (1 << k + 1)))
-                p[i] = k;
-            ++k;
-        }
+        p[1] = 0;
+        rep(i, 2, n+1) p[i] = p[i >> 1] + 1;
     }
-
-    int st(int L, int R) {
+    int query(int L, int R) {
         int k = p[R - L + 1];
         return min(d[L][k], d[R - (1 << k) + 1][k]);
     }
