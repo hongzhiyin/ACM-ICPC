49b6ca6e9cdc99b42e64308d991eeab2f02ea4d5
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-30 17:50:02 +0800

    Update Sparse Table.cpp

--- template/data structure/Sparse Table.cpp
+++ template/data structure/Sparse Table.cpp
@@ -1,16 +1,17 @@
 // 固定起点的区间的最值具有单调性，可依此进行二分
 
 struct ST {
-    int d[N][30], p[N];
+    int d[N][30], lg[N];
     void init(int *a, int n) {      // a[] 从 0 开始
-        rep(i, 0, n) d[i][0] = a[i];
-        for (int j = 1; (1 << j) <= n; ++j)
-            for (int i = 0; i + (1 << j) - 1 < n; ++i)
+        rep(i, 1, n+1) d[i][0] = a[i];
+        for (int j = 1; j < 30; ++j)
+            for (int i = 1; i + (1 << j) - 1 <= n; ++i)
                 d[i][j] = min(d[i][j - 1], d[i + (1 << (j - 1))][j - 1]);
-        p[1] = 0; rep(i, 2, n+1) p[i] = p[i >> 1] + 1;
+        lg[1] = 0; rep(i, 2, n+1) lg[i] = lg[i >> 1] + 1;
     }
     int query(int L, int R) {       // 看情况 L 和 R 是否需要提前 -1
-        int k = p[R - L + 1];
+        int k = lg[R - L + 1];
         return min(d[L][k], d[R - (1 << k) + 1][k]);
     }
 };
+
