3ac430e21156508666ac5177149ce49aba482aaa
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-30 16:58:33 +0800

    Update Binary Search.cpp

--- template/binary search/Binary Search.cpp
+++ template/binary search/Binary Search.cpp
@@ -1,4 +1,6 @@
-//求最大值
+// 最终的二分值 L 未必符合期望，应该带入公式判断是否正确。
+
+// 求最大值
 int L = 0, R = max;
 while (L < R) {
     int M = L + ((R - L + 1) >> 1);     //防止溢出，溢出可能造成TLE
@@ -6,7 +8,7 @@ while (L < R) {
 }
 printf("%d\n", L);
 
-//求最小值
+// 求最小值
 int L = 0, R = max;
 while (L < R) {
     int M = L + ((R - L) >> 1);
@@ -14,14 +16,14 @@ while (L < R) {
 }
 printf("%d\n", L);
 
-//浮点数
+// 浮点数
 db L = 0, R = max;
 while (!eq(L, R)) {
     db M = (L + R) / 2;
     if (check(M)) L = M;
     else R = M;
 }
-printf("%.4f\n", L);    // L 不行就换 R 试试，精度问题，谁知道呢
+printf("%.4f\n", 0.5 * (L + R));    // L 不行就换 R 试试，精度问题，谁知道呢
 
 // 各种莫名其妙的精度问题：
 // 保留两位小数，其余位舍去：
