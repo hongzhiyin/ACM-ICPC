9051154cf669f6c8412ab7b1a5b7ca533b2aedc4
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-07-31 09:45:48 +0800

    Update and rename template/binary search/Least Common Ancestors.cpp to template/Tree/Least Common Ancestors.cpp

--- template/Tree/Least Common Ancestors.cpp
+++ template/Tree/Least Common Ancestors.cpp
@@ -1,3 +1,5 @@
+// 倍增
+vi e[N];
 int f[N][30], dep[N];
 struct LCA {
     void init() { memset(f, 0, sizeof(f)); memset(dep, 0, sizeof(dep)); }   // 如果不用 init() 可能可以省空间
@@ -19,3 +21,26 @@ struct LCA {
         return f[u][0];
     }
 };
+
+// 离线 tarjan
+vi e[N];
+vector <pii> q[N];  // 事先存下询问
+bool vis[N];
+int fa[N], lca[N];  // 答案存在 lca[] ，下标为询问序号
+struct LCA {
+    void init() { memset(vis, 0, sizeof(vis)); };
+    int find(int x) { return x == fa[x] ? x : fa[x] = find(fa[x]); }
+    void dfs(int u) {
+        fa[u] = u;
+        vis[u] = 1;
+        rep(i, 0, sz(e[u])) {
+            int v = e[u][i];
+            if (!vis[v]) dfs(v), fa[v] = u;
+        }
+        rep(i, 0, sz(q[u])) {
+            int v = q[u][i].fi;
+            if (vis[v]) lca[q[u][i].se] = find(v);
+        }
+    }
+};
+LCA obj;
