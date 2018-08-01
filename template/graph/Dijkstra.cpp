4e230b95e90c0b61a66323cd6767ee2d925fae31
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-01 11:59:58 +0800

    Update Dijkstra.cpp

--- template/graph/Dijkstra.cpp
+++ template/graph/Dijkstra.cpp
@@ -1,33 +1,24 @@
-struct Dijkstra {   //注意：点的标号是从 0 开始 rep(i,0,n)
-    int n;
-    ll dist[N];
-    bool done[N];
-    vector < pair<int, ll> > e[N];
-
-    void Init(int n) {  //从 1 开始就传入 n + 1
-        this->n = n;
-        rep(i, 0, n) e[i].clear();
-    }
-
-    void AddEdge(int u, int v, ll w) {
-        e[u].pb({v, w});
-    }
-
-    void dijkstra(int s) {
-        priority_queue < pair<ll, int> > Q;
-        rep(i,0,n) dist[i] = LINF;
+int dist[N];
+bool done[N];
+vector <pii> e[N];
+priority_queue <pii> Q;
+struct Dijkstra {
+    void init() {
+        while (!Q.empty()) Q.pop();
+        memset(dist, 0x3f, sizeof(dist));
         memset(done, 0, sizeof(done));
-        Q.push({dist[s] = 0, s});
+    }
+    void dijkstra(int s) {
+        Q.push(mp(dist[s] = 0, s));
         while (!Q.empty()) {
             int u = Q.top().se; Q.pop();
             if (done[u]) continue;
             done[u] = true;
             rep(i, 0, sz(e[u])) {
-                int v = e[u][i].fi;
-                ll w = e[u][i].se;
+                int v = e[u][i].fi, w = e[u][i].se;
                 if (dist[v] > dist[u] + w) {
                     dist[v] = dist[u] + w;
-                    Q.push({-dist[v], v});
+                    Q.push(mp(-dist[v], v));
                 }
             }
         }
