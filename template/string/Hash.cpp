97958f42c1db72ff8b5b014106984b1da07674c8
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-08 15:30:35 +0800

    Update Hash.cpp

--- template/string/Hash.cpp
+++ template/string/Hash.cpp
@@ -1,18 +1,13 @@
+typedef unsigned long long ull;
 char s[N];
-ull Hash[N], p[N];
-set <ull> S;
-void Init()
-{
-    S.clear();
-    ull base = 2333333333333333;
-    p[0] = 1;
-    Hash[0] = 0;
-    int len = strlen(s + 1);
-    rep(i, 1, N) p[i] = p[i-1] * base;
-    rep(i, 1, len+1) Hash[i] = Hash[i-1] * base + (s[i] - 'a' + 1);
-}
-
-ull GetHash(int l, int r)
-{
-    return Hash[r] - Hash[l-1] * p[r-l+1];
-}
+ull ha[N], p[N];      // ha[i] 表示从 1 到 i 位的字符串映射为数字， p[i] 表示 base 的 i 次方
+struct Hash {
+    ull base;
+    void init(char *s, int len) {       // s[] 从 1 开始
+        base = 2333333333333;
+        p[0] = 1; ha[0] = 0;
+        rep(i, 1, N) p[i] = p[i-1] * base;
+        rep(i, 1, len+1) ha[i] = ha[i-1] * base + s[i];
+    }
+    ull query(int l, int r) { return ha[r] - ha[l-1] * p[r-l+1]; }
+};
