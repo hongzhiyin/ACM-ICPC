363b3f80019a64b1e4fa6721c2ea0f8c2e66dfce
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-10 02:22:18 +0800

    Update Matrix.cpp

--- template/math/Matrix.cpp
+++ template/math/Matrix.cpp
@@ -1,29 +1,29 @@
 struct Mat {
-	vector<vi> a;
-	Mat() {}
-	Mat(int n, int m) { a.resize(n, vi(m)); }
-	Mat operator *(const Mat &b) const {
-		int n = sz(a), m = sz(b.a[0]), nm = sz(b.a); Mat r(n, m);
-		rep(i, 0, n) rep(j, 0, m) rep(k, 0, nm)
-			r.a[i][j] = add(r.a[i][j], mul(a[i][k], b.a[k][j]));
-		return r;
-	}
-	Mat operator ^(ll b) {
-		int n = sz(a); Mat r(n, n), t = *this;
-		rep(i, 0, sz(r.a)) r.a[i][i] = 1;
-		for (; b; b >>= 1) {
-			if (b & 1) r = r * t;
-			t = t * t;
-		}
-		return r;
-	}
-	void out() {
-		rep(j, 0, sz(a)) {
-		    vi r = a[j];
-		    rep(i, 0, sz(r))
-                	printf("%d%c", r[i], " \n"[i == sz(r) - 1]);
-        	}
-	}
+    vector<vi> a;
+    Mat() {}
+    Mat(int n, int m) { a.resize(n, vi(m)); }
+    Mat operator *(const Mat &b) const {
+        int n = sz(a), m = sz(b.a[0]), nm = sz(b.a); Mat r(n, m);
+        rep(i, 0, n) rep(j, 0, m) rep(k, 0, nm)
+            r.a[i][j] = add(r.a[i][j], mul(a[i][k], b.a[k][j]));    // 取模运算十分耗时，尽可能最后再取模
+        return r;
+    }
+    Mat operator ^(ll b) {
+        int n = sz(a); Mat r(n, n), t = *this;
+        rep(i, 0, sz(r.a)) r.a[i][i] = 1;
+        for (; b; b >>= 1) {
+            if (b & 1) r = r * t;
+            t = t * t;
+        }
+        return r;
+    }
+    void out() {
+        rep(j, 0, sz(a)) {
+            vi r = a[j];
+            rep(i, 0, sz(r))
+                printf("%d%c", r[i], " \n"[i==sz(r)-1]);
+        }
+    }
 };
 
 /*
