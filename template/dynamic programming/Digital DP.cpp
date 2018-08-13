1f08d75207109d2b5e5ccc9e9989f9abea16b14a
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-13 13:54:52 +0800

    Update Digital DP.cpp

--- template/dynamic programming/Digital DP.cpp
+++ template/dynamic programming/Digital DP.cpp
@@ -1,27 +1,24 @@
-// hdu 2089
-// 求区间内不包含 4 和 62 的数字个数
-// https://www.cnblogs.com/wenruo/p/4725005.html
+// https://blog.csdn.net/wust_zzwh/article/details/52100392
 
-void Pre() {    // dp[i][j] 表示长度为 i 位，首位为 j 的符合要求的数字个数
-    dp[0][0] = 1;   // 初始化
-    rep(i, 1, 8) rep(j, 0, 10) rep(k, 0, 10)
-        if (j != 4 && !(j == 6 && k == 2))    // 合法情况
-          dp[i][j] += dp[i-1][k];   // 长度为 i 位，首位为 j 的数字个数等于合法的长度为 i-1 位，首位为 k 的数字个数
+memset(dp, -1, sizeof(dp));     // 如果多组数据不影响 dp[][] ，就放在 while() 外面
+
+int dp[ 数位长度 ][ 状态数 ], num[ 数位长度 ];
+int dfs(int pos, int state, bool limit) {   // pos ：当前枚举位， state ：当前状态， limit ：当前枚举位是否有限制
+    if (pos == -1) return 1;                // 枚举完毕
+    if (!limit && dp[pos][state] != -1) return dp[pos][state];    // 记忆化搜索
+    int ans = 0, up = limit ? num[pos] : 9;          // 确定上界
+    rep(i, 0, up+1) {
+        if (!check(state, i)) continue;     // 状态非法
+        ans += dfs(pos-1, change(state), limit && i==num[pos]);
+    }
+    return limit ? ans : dp[pos][state] = ans;
 }
 
-int work(int x) {   // 求 [0, x) 中合法的数字个数
-    int len = 0, res = 0;
+int digit(int x) {
+    int pos = 0;
     while (x) {
-        d[len++] = x % 10;
+        num[pos++] = x % 10;
         x /= 10;
     }
-    d[len + 1] = 0;
-    per(i, 1, len+1) {    // 从高位到低位遍历
-        rep(j, 0, dig[i]) {   // 遍历最高位小于当前枚举位的长度为 i 的合法数字个数
-            if (!(dig[i+1] == 6 && j == 2))
-                res += dp[i][j];
-        }
-        if (dig[i] == 4 || dig[i+1] == 6 && dig[i] == 2) break;   // 非法情况直接退出循环
-    }
-    return res;
+    return dfs(pos-1, state, true);
 }
