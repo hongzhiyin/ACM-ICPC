030c96a9807f4f6711c404b11e983e0c4a0dc097
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-18 09:24:43 +0800

    Create Interaction.cpp

---
+++ template/base/Interaction.cpp
@@ -0,0 +1,7 @@
+int query(int sx, int sy, int ex, int ey){
+    printf("? %d %d %d %d\n", sx, sy, ex, ey);
+    fflush(stdout);
+    string s; cin >> s;
+    if(s == "YES") return 1;
+    return 0;
+}
