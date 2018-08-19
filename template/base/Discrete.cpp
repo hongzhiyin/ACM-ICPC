a8b1da81a0f44163d9392f358fb5c3e22da6effd
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-20 00:19:26 +0800

    Update Discrete.cpp

--- template/base/Discrete.cpp
+++ template/base/Discrete.cpp
@@ -2,14 +2,13 @@
 vector <Node> a;
 vector <T> dis;
 void Init() {
-    a.clear(); dis.clear(); dis.pb(-INF);
-    T x, y, z;
+    a.clear(); dis.clear(); dis.pb(-INF);   // 离散值从 1 开始
     rep(i, 0, n) {
         scanf("%d%d%d", &x, &y, &z);
         a.pb(Node(x, y, z));
         dis.pb(x);
     }
-    sort(all(a)); sort(all(dis));
+    sort(all(dis));
     dis.erase(unique(all(dis)), dis.end());
     rep(i, 0, sz(a))
         a[i].dx = lower_bound(all(dis), a[i].x) - dis.begin();
