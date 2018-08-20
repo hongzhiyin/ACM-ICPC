32610fcdb5cd53455243100c36839a14b9050f32
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-08-20 23:11:29 +0800

    Update Prime.cpp

--- template/math/Prime.cpp
+++ template/math/Prime.cpp
@@ -13,14 +13,27 @@ void GetPrime(int n)    // 预处理 [2, n] 的质数
     }
 }
 
-// n <= 1e18
-// a ^ (n-1) = 1 (mod n)
-bool check(ll x) {
-    if (n == 2) return 1;
-    if (n < 2 || !(n & 1)) return 0;
+// x <= 1e18
+// a ^ (x-1) = 1 (mod x)
+bool check(ll x) {      // 判断 x 是否是质数
+    if (x == 2) return 1;
+    if (x < 2 || !(x & 1)) return 0;
     rep(t, 0, 10) {
-        ll a = rand() % (n-1) + 1;
-        if (powmod(a, n-1) != 1) return 0;
+        ll a = rand() % (x-1) + 1;
+        if (powmod(a, x-1) != 1) return 0;
     }
     return 1;
+}
+
+set <int> S;
+void DPF(int x) {       // Decompose Prime Factor 分解质因数
+    rep(i, 0, sz(prim)) {   // 预处理 [2, sqrt(x)] 的质数
+        int p = prim[i];
+        if (p > x) break;
+        if (x % p == 0) {
+            S.insert(p);
+            while (x % p == 0) x /= p;
+        }
+    }
+    if (x != 1) S.insert(x);
 }
