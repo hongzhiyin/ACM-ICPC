439bc9802efc6c00d9a0eadcbaa9b3ca629c6aac
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-01 10:05:02 +0800

    Update MinCost.cpp

--- template/graph/MinCost.cpp
+++ template/graph/MinCost.cpp
@@ -3,19 +3,19 @@ struct MinCost {
         int s, t, cap, cost, net;
         Edge() {}
         Edge(int s, int t, int cap, int cost, int net) : s(s), t(t), cap(cap), cost(cost), net(net) {}
-	} e[M<<1];
-	ll dis[N];
-	queue <int> Q;
-	int n, no, pre[N], vis[N], head[N];
-	void init(int _n) {      // 最大点编号 + 1
+    } e[M<<1];
+    ll dis[N];
+    queue <int> Q;
+    int n, no, pre[N], vis[N], head[N];
+    void init(int _n) {      // 最大点编号 + 1
         n = _n, no = 0;
         memset(head, -1, sizeof(head[0]) * n);
-	}
-	void addEdge(int s, int t, int cap, int cost) {
+    }
+    void addEdge(int s, int t, int cap, int cost) {
         e[no] = Edge(s, t, cap, cost, head[s]), head[s] = no++;
         e[no] = Edge(t, s, 0, -cost, head[t]), head[t] = no++;
-	}
-	bool bfs(int S, int T) {
+    }
+    bool bfs(int S, int T) {
         rep(i, 0, n) pre[i] = -1, dis[i] = LINF, vis[i] = false;
         dis[S] = 0, vis[S] = true, Q.push(S);
         while (!Q.empty()) {
@@ -30,8 +30,8 @@ struct MinCost {
             vis[u] = false;
         }
         return dis[T] != LINF;
-	}
-	pair<ll, ll> solve(int S, int T) {
+    }
+    pair<ll, ll> solve(int S, int T) {
         ll maxflow = 0, mincost = 0;
         while (bfs(S, T)) {
             ll flow = LINF;
@@ -40,5 +40,5 @@ struct MinCost {
             maxflow += flow, mincost += flow * dis[T];
         }
         return mp(maxflow, mincost);
-	}
+    }
 };
