cdb1fd8ca5853a4795d0f729496d6d799a3be44d
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-04 17:37:25 +0800

    Update Adjacency List.cpp

--- template/data structure/Adjacency List.cpp
+++ template/data structure/Adjacency List.cpp
@@ -1,11 +1,11 @@
-T eno[N];      // 无向边 : N << 1
+T node[N];      // 无向边 : N << 1
 int no, net[N];
 struct Vector {
     int head;
     Vector() { head = 0; }
     void clear() { head = 0; }
     void push_back(T x) {
-        eno[++no] = x; net[no] = head; head = no;
+        node[++no] = x; net[no] = head; head = no;
     }
 };
 Vector e[N];
@@ -14,4 +14,4 @@ void Init() {
 }
 // 遍历
 for (int i = e[u].head; i; i = net[i]) {
-    T v = eno[i];
+    T v = node[i];
