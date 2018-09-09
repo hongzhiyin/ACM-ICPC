e18ae68de72c724d5d6c1d21d2352a48ba64c1b8
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-09 10:51:54 +0800

    Update Dijkstra.cpp

--- template/graph/Dijkstra.cpp
+++ template/graph/Dijkstra.cpp
@@ -2,29 +2,22 @@ int dist[N];
 bool done[N];
 vector <pii> e[N];
 priority_queue <pii> Q;
-struct Dijkstra {
-    void init(int n) {
-        while (!Q.empty()) Q.pop();
-        memset(dist, 0x3f, sizeof(dist));
-        memset(done, 0, sizeof(done));
-        rep(i, 0, n+1) e[i].clear();
-    }
-    void dijkstra(int s) {
-        Q.push(mp(dist[s] = 0, s));
-        while (!Q.empty()) {
-            int u = Q.top().se; Q.pop();
-            if (done[u]) continue;
-            done[u] = true;
-            rep(i, 0, sz(e[u])) {
-                int v = e[u][i].fi, w = e[u][i].se;
-                if (dist[v] > dist[u] + w) {
-                    dist[v] = dist[u] + w;
-                    Q.push(mp(-dist[v], v));
-                }
+void dijkstra(int s) {
+    memset(dist, 0x3f, sizeof(dist));
+    memset(done, 0, sizeof(done));
+    Q.push(mp(dist[s] = 0, s));
+    while (!Q.empty()) {
+        int u = Q.top().se; Q.pop();
+        if (done[u]) continue; done[u] = true;
+        rep(i, 0, sz(e[u])) {
+            int v = e[u][i].fi, w = e[u][i].se;
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                Q.push(mp(-dist[v], v));
             }
         }
     }
-};
+}
 
 ================================================== Problem Set ==================================================
 
