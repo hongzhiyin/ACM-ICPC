b08e2178152080e6b9f39d23e458cb95e9529f09
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-10 21:07:45 +0800

    Update MaxFlow.cpp

--- template/graph/MaxFlow.cpp
+++ template/graph/MaxFlow.cpp
@@ -8,7 +8,7 @@ struct MaxFlow {
     } e[M<<1];
     void init(int _n) {     // _n 为最大点编号 + 1
         n = _n, no = 0;
-        memset(head, -1, sizeof(head[0]) * n);
+        memset(head, -1, sizeof(head[0]) * n);  // 去掉结构体会影响到这里和下面的 n
     }
     void addEdge(int s, int t, int v) {
         e[no] = Edge(s, t, v, head[s]), head[s] = no++;
@@ -54,3 +54,31 @@ struct MaxFlow {
         return maxflow;
     }
 };
+
+================================================== Problem Set ==================================================
+
+// luogu P2756
+// 题意：二分图最大匹配，输出方案
+// 题解：建超级源点和超级汇点，分别连流量为 1 的边，跑最大流
+int Solve() {
+    obj.init(n+2);  // 最大点编号 + 1
+    int u, v;
+    while (~scanf("%d%d", &u, &v)) {
+        if (u == -1 || v == -1) break;
+        obj.addEdge(u, v, 1);
+    }
+    S = 0, T = n + 1;
+    rep(i, 1, m+1) obj.addEdge(S, i, 1);
+    rep(i, m+1, n+1) obj.addEdge(i, T, 1);
+
+    ll ans = obj.dinic(S, T);
+    if (!ans) return puts("No Solution!");
+    printf("%lld\n", ans);
+    for (int i = 0; i < obj.no; i += 2) {
+        if (obj.e[i].s != S && obj.e[i].t != S) {   // 如果不是连接源点的边（没判断汇点是因为 break ）
+            if (obj.e[i^1].v != 0)  // 如果反向边流量不为 0 ，说明该正向边有流量通过
+                printf("%d %d\n", obj.e[i].s, obj.e[i].t);
+        } else break;   // 因为加边的顺序，当出现连接源点的边时，说明已经遍历所有匹配边
+    }
+    return 0;
+}
