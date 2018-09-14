a89ad620b411e937788730b8a0c69825dc5879bf
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-14 21:23:38 +0800

    Update Palindromic Tree.cpp

--- template/string/Palindromic Tree.cpp
+++ template/string/Palindromic Tree.cpp
@@ -102,3 +102,39 @@ int Solve() {
     rep(i, 0, n) printf("%d%c", ans[i], " \n"[i==n-1]);
     return 0;
 }
+
+----------------------------------------------------------------------------------------------------
+
+// ural 1960
+// 题意：每加一个字符，统计本质不同的回文串个数
+int Solve() {
+    obj.init();
+    int len = strlen(s);
+    rep(i, 0, len) {
+        obj.add(s[i]);
+        printf("%d%c", obj.no - 2, " \n"[i==len-1]);
+    }
+    return 0;
+}
+
+----------------------------------------------------------------------------------------------------
+
+
+// 题意：找到最长的串 XY ， X 为长度非 0 的回文串， Y 为长度非 0 的回文串
+// 题解：枚举以 i 为结尾的最长回文长度和以 i 为开头的最长回文长度
+int Solve() {
+    obj.init();
+    int len = strlen(s);
+    rep(i, 0, len) {
+        obj.add(s[i]);
+        pre[i] = obj.len[obj.last];
+    }
+    int ans = 0;
+    obj.init();
+    per(i, 1, len) {
+        obj.add(s[i]);
+        suf[i] = obj.len[obj.last];
+        ans = max(ans, pre[i-1] + suf[i]);
+    }
+    return printf("%d\n", ans);
+}
