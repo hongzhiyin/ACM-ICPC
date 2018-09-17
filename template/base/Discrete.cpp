61c65dcca8d1a9564c1a5451bf4e0c9dc3af66a2
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-09-17 20:39:18 +0800

    Update Discrete.cpp

--- template/base/Discrete.cpp
+++ template/base/Discrete.cpp
@@ -1,15 +1,8 @@
-// 将 a 的数据成员 x 离散化为 dx
-vector <Node> a;
-vector <T> dis;
+vi dis;
 void Init() {
-    a.clear(); dis.clear(); dis.pb(-INF);   // 离散值从 1 开始
-    rep(i, 0, n) {
-        scanf("%d%d%d", &x, &y, &z);
-        a.pb(Node(x, y, z));
-        dis.pb(x);
-    }
+    dis.clear();
+    rep(i, 0, n) scanf("%d", a+i), dis.pb(a[i]);
     sort(all(dis));
     dis.erase(unique(all(dis)), dis.end());
-    rep(i, 0, sz(a))
-        a[i].dx = lower_bound(all(dis), a[i].x) - dis.begin();
+    rep(i, 0, n) d[i] = lower_bound(all(dis), a[i]) - dis.begin() + 1;  // 离散值从 1 开始
 }
