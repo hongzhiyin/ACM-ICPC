8d7c748f5f63fddeb30a0407821bc5e33ef65c3b
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-10-10 09:25:13 +0800

    Update STL.cpp

--- template/base/STL.cpp
+++ template/base/STL.cpp
@@ -7,3 +7,8 @@ cnt = it.se - it.fi
 // 消除字符串 s 首尾空格
 s.erase(0, s.find_first_not_of(" "));
 s.erase(s.find_last_not_of(" ") + 1);
+
+// 分割字符串 s
+int pos = s.find("  ");
+s1 = s.substr(0, pos);
+s2 = s.substr(pos, sz(str)-pos);
