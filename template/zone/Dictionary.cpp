d018a7a56aa278e3ee4be8f7e8b5c1ca93d73c02
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-10-18 20:01:21 +0800

    Update Dictionary.cpp

--- template/zone/Dictionary.cpp
+++ template/zone/Dictionary.cpp
@@ -1,1 +1,87 @@
 
+# A
+
+adore                         	崇拜
+
+# B
+
+
+# C
+
+
+# D
+
+
+# E
+
+
+# F
+
+
+# G
+
+
+# H
+
+hence                         	于是
+
+# I
+
+
+# J
+
+
+# K
+
+
+# L
+
+
+# M
+
+
+# N
+
+
+# O
+
+
+# P
+
+postpone                      	推迟
+progress                      	进展
+pyramid                       	金字塔
+
+# Q
+
+
+# R
+
+recruits                      	新兵
+
+# S
+
+shovel                        	铲
+simultaneously                	同时
+
+# T
+
+
+# U
+
+
+# V
+
+vending machine               	自动售货机
+
+# W
+
+
+# X
+
+
+# Y
+
+
+# Z
+
