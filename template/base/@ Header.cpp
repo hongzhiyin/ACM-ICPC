04439815b8cba677813490f361edbd237f02b550
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2018-11-06 10:26:05 +0800

    Update @ Header.cpp

--- template/base/@ Header.cpp
+++ template/base/@ Header.cpp
@@ -1,3 +1,4 @@
+/*<<head>> {{{*/
 #include <bits/stdc++.h>
 #include <algorithm>
 #include <iostream>
@@ -29,6 +30,7 @@ typedef pair<int,int> pii;
 //LINF = 4 557 430 888 798 830 399 ; LINF + LINF = 9 114 861 777 597 660 798 ;
 const int INF = 0x3f3f3f3f;     // -INF: 0xc0   abs(-INF) = INF + 1
 const ll LINF = 0x3f3f3f3f3f3f3f3f;
+/*}}}*/                         // zr, zm
 const int MOD = (int)1e9+7;     // when MOD = 1e9+7, inv2 = 500000004
 const int N = (int)-1;
 
