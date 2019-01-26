c1d4256d1dfa0ecfd4783a0f84a64006672dfcc2
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-27 01:27:41 +0800

    Update Quick Sort.cpp

--- template/sort/Quick Sort.cpp
+++ template/sort/Quick Sort.cpp
@@ -1,4 +1,4 @@
-void qsort(int l, int r) {
+void qsort(int a[], int l, int r) {
     if (l >= r) return ;
     int i = l, j = r;
     ll mid = a[(l+r)>>1];
@@ -7,6 +7,6 @@ void qsort(int l, int r) {
         while (a[j] > mid) --j;
         if (i <= j) swap(a[i++], a[j--]);
     }
-    qsort(l, j);
-    qsort(i, r);
+    qsort(a, l, j);
+    qsort(a, i, r);
 }
