598c09f401e02b13547594de3567bf930a19fa60
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-27 09:21:55 +0800

    Update Prime.cpp

--- template/math/Prime.cpp
+++ template/math/Prime.cpp
@@ -4,12 +4,21 @@
 // prim 数组中的素数是递增的，当 i 能整除 prim[j]，那么 i*prim[j+1] 这个合数肯定被 prim[j] 乘以某个数筛掉
 // 因为 i 中含有 prime[j], prime[j] 比 prime[j+1] 小。接下去的素数同理，所以不用筛下去了
 // 在满足 i % prim[j] == 0 这个条件之前以及第一次满足该条件时， prim[j] 必定是 prim[j]*i 的最小因子
+
+/*
+时间复杂度 : O(n)
+输入 :
+    n : 筛出 [2, n) 内的质数
+输出 :
+    prim : 质数集合 ; notPrim[i] : i 是否为质数
+*/
+
 vi prim;
 bool notPrim[N];
 void GetPrime(int n) {   // 预处理 [2, n) 的质数
     memset(notPrim, 0, sizeof(notPrim)); prim.clear();
     notPrim[0] = notPrim[1] = 1;
-    rep(i, 2, n+1) {
+    rep(i, 2, n) {
         if (!notPrim[i]) prim.pb(i);
         for (int j = 0; j < sz(prim) && (ll)i * prim[j] < n; ++j) {
             notPrim[i*prim[j]] = 1;
