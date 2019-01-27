9e6cd293e38836ba2a46cb02301df158073443b6
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-27 20:50:32 +0800

    Update Kruskal.cpp

--- template/graph/Kruskal.cpp
+++ template/graph/Kruskal.cpp
@@ -1,16 +1,26 @@
-struct Kruskal {
-    int Find(int x) { return fa[x] == x ? x : fa[x] = Find(fa[x]); }
-    void Union(int x, int y) { fa[find(x)] = find(y); }
-    ll kruskal() {
-        ll sum = 0;
-        sort(all(e));
-        rep(i,0,sz(e)) {
-            int u = e[i].se.fi, v = e[i].se.se;
-            if (Find(u) != Find(v)) {
-                sum += e[i].fi;
-                Union(u, v);
-            }
+/*
+时间复杂度 : O( ElogE or ElogV )
+准备 :
+    e : 边集 ; fa[] : 并查集
+输入 :
+输出 :
+    sum : 最小生成树的边权和
+注意 :
+*/
+
+int fa[N];
+vector< pair<ll, pii> > e; // 边集
+int Find(int x) { return fa[x] == x ? x : fa[x] = Find(fa[x]); }
+void Union(int x, int y) { fa[Find(x)] = Find(y); }
+ll kruskal() {
+    ll sum = 0;
+    sort(all(e));
+    rep(i,0,sz(e)) {
+        int u = e[i].se.fi, v = e[i].se.se;
+        if (Find(u) != Find(v)) {
+            sum += e[i].fi;
+            Union(u, v);
         }
-        return sum;     //返回最小生成树的边权和
     }
-};
+    return sum;     //返回最小生成树的边权和
+}
