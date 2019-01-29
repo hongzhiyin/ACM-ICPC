bb41e8b07906026ce92623b26da7bc62630ce7a3
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-29 21:23:14 +0800

    Update Sparse Table.cpp

--- template/data structure/Sparse Table.cpp
+++ template/data structure/Sparse Table.cpp
@@ -1,19 +1,25 @@
-// 固定起点的区间的最值具有单调性，可依此进行二分
+/*
+【函数功能】
+void Init(T a[], int n) : 以长度为 n 的数组 a[] 初始化 ST 表
+T Qry(int L, int R)     : 询问区间 [L, R] 的最值
 
+【注意事项】
+1. a[] 从 1 开始
+2. 根据需要修改 min or max
+3. 固定起点的区间的最值具有单调性，可依此进行二分
+*/
+
+template <class T>
 struct ST {
-    int d[N][30], lg[N];
-    void init(int *a, int n) {      // a[] 从 0 开始
-        rep(i, 0, n) d[i][0] = a[i];
-        // rep(i, 1, n+1) d[i][0] = a[i];  // a[] 从 1 开始
-        for (int j = 1; j < 30; ++j)
-            for (int i = 0; i + (1 << j) - 1 < n; ++i)
-            // for (int i = 1; i + (1 << j) - 1 <= n; ++i)  // a[] 从 1 开始
-                d[i][j] = min(d[i][j - 1], d[i + (1 << (j - 1))][j - 1]);
-        lg[1] = 0; rep(i, 2, n+1) lg[i] = lg[i >> 1] + 1;
+    int lg[N]; T d[N][30];
+    void Init(T a[], int n) {
+        rep(i, 1, n+1) d[i][0] = a[i];
+        rep(j, 1, 30) for (int i = 1; i + (1 << j) - 1 <= n; ++i)
+            d[i][j] = max(d[i][j-1], d[i + ( 1 << (j-1) )][j-1]);
+        lg[1] = 0; rep(i, 2, n+1) lg[i] = lg[i>>1] + 1;
     }
-    int query(int L, int R) {       // 看情况 L 和 R 是否需要提前 -1
+    T Qry(int L, int R) {
         int k = lg[R - L + 1];
-        return min(d[L][k], d[R - (1 << k) + 1][k]);
+        return max(d[L][k], d[R - (1<<k) + 1][k]);
     }
 };
-
