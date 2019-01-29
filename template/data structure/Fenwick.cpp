f266922900a81571cc870f23e5d5a1fbd5ed55c7
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-29 19:56:00 +0800

    Update Fenwick.cpp

--- template/data structure/Fenwick.cpp
+++ template/data structure/Fenwick.cpp
@@ -26,23 +26,30 @@ struct Fenwick {
     void Add(int x, T d) { for (; x <= n; x += x&-x) t[x] += d; }
 };
 
-// 区间修改，区间查询
+/* 区间修改，区间查询
+【函数功能】
+void Init(int a[], int n)   : 根据大小为 n 的数组 a[] 初始化树状数组
+void Upd(int l, int r, T x) : 区间 [l, r] 内元素都加上 x
+T Qry(int l, int r)         : 返回区间 [l, r] 内元素之和
+*/
+
+template <class T>
 struct Sec_Fenwick {
-    Fenwick c1, c2;
-    void Init(ll *a, int n) {    // a[] 下标从 1 开始， a[0] = 0
+    Fenwick <T> c1, c2;
+    void Init(T a[], int n) {    // a[] 下标从 1 开始， a[0] = 0
         c1.Init(n); c2.Init(n);
         rep(i, 1, n+1) {
             c1.Add(i, a[i] - a[i-1]);
             c2.Add(i, (i-1) * (a[i] - a[i-1]));
         }
     }
-    void Update(ll l, ll r, ll x) {
+    void Upd(int l, int r, T x) {
         c1.Add(l, x); c1.Add(r+1, -x);
         c2.Add(l, (l-1) * x); c2.Add(r+1, r * -x);
     }
-    ll Query(ll l, ll r) {
-        ll R = r * c1.Sum(r) - c2.Sum(r);
-        ll L = (l-1) * c1.Sum(l-1) - c2.Sum(l-1);
+    T Qry(int l, int r) {
+        T R = r * c1.Sum(r) - c2.Sum(r);
+        T L = (l-1) * c1.Sum(l-1) - c2.Sum(l-1);
         return R - L;
     }
 };
