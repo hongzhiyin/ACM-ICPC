86d6acfff0f2610b44ec07f73ef00f60414b97fa
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-29 22:44:41 +0800

    Update Matrix.cpp

--- template/math/Matrix.cpp
+++ template/math/Matrix.cpp
@@ -1,11 +1,13 @@
+int add(ll a, int b) { if ((a += b) >= MOD) a -= MOD; return a; }
+int mul(ll a, int b) { if ((a *= b) >= MOD) a %= MOD; return a; }
 struct Mat {
     vector<vi> a;
     Mat() {}
     Mat(int n, int m) { a.resize(n, vi(m)); }
-    Mat operator *(const Mat &b) const {
+    Mat operator * (Mat &b) {
         int n = sz(a), m = sz(b.a[0]), nm = sz(b.a); Mat r(n, m);
         rep(i, 0, n) rep(j, 0, m) rep(k, 0, nm)
-            r.a[i][j] = add(r.a[i][j], mul(a[i][k], b.a[k][j]));    // 取模运算十分耗时，尽可能最后再取模
+            r.a[i][j] = add(r.a[i][j], mul(a[i][k], b.a[k][j]));
         return r;
         /*  当 r.a[i][j] 的最大值不会溢出时
         rep(i, 0, n) rep(j, 0, m) {
@@ -14,7 +16,7 @@ struct Mat {
         }
         */
     }
-    Mat operator ^(ll b) {
+    Mat operator ^ (ll b) {
         int n = sz(a); Mat r(n, n), t = *this;
         rep(i, 0, sz(r.a)) r.a[i][i] = 1;
         for (; b; b >>= 1) {
@@ -24,11 +26,8 @@ struct Mat {
         return r;
     }
     void out() {
-        rep(j, 0, sz(a)) {
-            vi r = a[j];
-            rep(i, 0, sz(r))
-                printf("%d%c", r[i], " \n"[i==sz(r)-1]);
-        }
+        for (auto r : a) rep(i, 0, sz(r))
+            printf("%d%c", r[i], " \n"[i==sz(r)-1]);
     }
 };
 
