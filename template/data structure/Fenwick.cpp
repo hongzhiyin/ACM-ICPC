cd91700ec5236907543d4ae01c618a9a18a2ee46
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-29 19:34:12 +0800

    Update Fenwick.cpp

--- template/data structure/Fenwick.cpp
+++ template/data structure/Fenwick.cpp
@@ -1,17 +1,29 @@
-struct Fenwick {    // 注意下标从 1 开始，以及可能需要离散化操作
-    int n; ll t[N]; // map <int, int> t;
+/*
+【函数功能】
+void Init(int n)     : 初始化大小为 n 的树状数组
+T Sum(int x)         : 返回前 x 个元素之和
+T Qry(int l, int r)  : 返回区间 [l, r] 内元素之和
+void Add(int x, T d) : 第 x 个元素加上 d
+
+【注意事项】
+1. 下标从 1 开始
+2. 如果下标过大，需要离散化
+*/
+
+template <class T>
+struct Fenwick {
+    int n; T t[N]; // map <int, int> t;
     void Init(int n) {
-        memset(t, 0, sizeof(t));
         this->n = n;
+        memset(t, 0, sizeof(t));
     }
-    ll Sum(int x) {
-        ll ret = 0;
+    T Sum(int x) {
+        T ret = 0;
         for (; x > 0; x -= x&-x) ret += t[x];
         return ret;
     }
-    void Add(int x, ll d) {
-        for (; x <= n; x += x&-x) t[x] += d;
-    }
+    T Qry(int l, int r) { return Sum(r) - Sum(l-1); }
+    void Add(int x, T d) { for (; x <= n; x += x&-x) t[x] += d; }
 };
 
 // 区间修改，区间查询
