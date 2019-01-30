7ae9908b644834c79723f2179d6d98d9d017a5bd
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-30 19:17:24 +0800

    Update Longest Increasing Subsequence.cpp

--- template/dynamic programming/Longest Increasing Subsequence.cpp
+++ template/dynamic programming/Longest Increasing Subsequence.cpp
@@ -1,6 +1,16 @@
-rep(i, 1, n+1) g[i] = INF;
-rep(i, 0, n) {
-    int k = lower_bound(g+1, g+n+1, a[i]) - g;
-    dp[i] = k;  //dp[i] 表示以 a[i] 结尾的最长子序列的长度
-    g[k] = a[i];    //用 a[i] 更新 g[] 中的值使得子序列元素尽可能小
+/*
+【注意事项】
+1. lower_bound : 严格上升 ; upper_bound : 非严格上升
+*/
+
+int g[N];
+int LIS(int a[], int n) {
+    int res = 0;
+    rep(i, 1, n+1) g[i] = INF;
+    rep(i, 0, n) {
+        int k = lower_bound(g+1, g+n+1, a[i]) - g;  // k 表示以 a[i] 结尾的最长子序列的长度
+        res = max(res, k);
+        g[k] = a[i];
+    }
+    return res; // 返回最长上升子序列长度
 }
