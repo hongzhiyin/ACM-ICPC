f5c148f638344a497670f5826f5567339911cfbc
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-01-30 20:31:30 +0800

    Update Lucas.cpp

--- template/math/Lucas.cpp
+++ template/math/Lucas.cpp
@@ -1,14 +1,21 @@
-// description : cal C(a, b) % MOD
-// cal-time : O( log_{mod}^{n} )
-// pre-time : O( MOD )
-// inv[i] : inv(i)
-// ijc[i] : inv(jc[i])
-// !!!! : MOD should be a prime
-int jc[MOD], ijc[MOD], inv[MOD];
-int C(int a, int b) { return a < b ? 0 : mul(jc[a], mul(ijc[b], ijc[a - b])); }
-int lucas(ll a, ll b) { return ! b ? 1 : mul(C(a % MOD, b % MOD), lucas(a / MOD, b / MOD)); }
-int lucas_ini() {
-	jc[0] = 1; rep(i, 1, MOD) jc[i] = mul(jc[i - 1], i);    //阶乘
-	inv[1] = 1; rep(i, 2, MOD) inv[i] = mul(inv[MOD % i], MOD - MOD / i);
-	ijc[0] = 1; rep(i, 1, MOD) ijc[i] = mul(ijc[i - 1], inv[i]);
+/*
+【复杂度】
+预处理 : O( log_{mod}^{n} ) // 底数 : p ; 真数 : n
+计算   : O( p )
+
+【注意事项】
+1. 记得先用 lucas_ini() 初始化
+2. 模数 p 必须是质数
+*/
+
+inline int add(ll a, int b) { if ((a += b) >= p) a -= p; return a; }
+inline int mul(ll a, int b) { if ((a *= b) >= p) a %= p; return a; }
+
+int p, jc[N], ijc[N], inv[N];  // jc : 阶乘 ; inv : 逆元 ; ijc : 阶乘的逆元
+void lucas_ini() {
+    jc[0] = 1; rep(i, 1, p) jc[i] = mul(jc[i-1], i);
+    inv[1] = 1; rep(i, 2, p) inv[i] = mul(inv[p%i], p - p / i);
+    ijc[0] = 1; rep(i, 1, p) ijc[i] = mul(ijc[i-1], inv[i]);
 }
+int C(int a, int b) { return a < b ? 0 : mul(jc[a], mul(ijc[b], ijc[a-b])); }
+int lucas(ll a, ll b) { return !b ? 1 : mul(C(a % p, b % p), lucas(a / p, b / p)); }
