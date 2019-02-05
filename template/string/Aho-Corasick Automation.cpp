e74cf6d029f8b1edcdb0e5d6d2e94febc982a678
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-02-05 18:13:29 +0800

    Update Aho-Corasick Automation.cpp

--- template/string/Aho-Corasick Automation.cpp
+++ template/string/Aho-Corasick Automation.cpp
@@ -1,58 +1,60 @@
-void init() {
-    node = -1; obj.newnode();
-}
+/*
+【使用方法】
+1. insert 插入模式串
+2. build  构建失配指针
+3. run    根据需求修改
+*/
 
-int node;
-int trie[N][128], fail[N], isw[N], last[N], que[N];
+void init() { no = -1; newnode(); }
+
+int no, trie[N][26], fail[N], isw[N], last[N], que[N];
 // N = 单词个数 * 单词长度，第二维视情况而定  last[] 表示上一个是单词结尾的失配位置
-struct AhoCorasick {
-    int newnode() {
-        ++node;
-        memset(trie[node], 0, sizeof(trie[node]));
-        fail[node] = 0;
-        isw[node] = 0;
-        return node;
+int newnode() {
+    ++no;
+    memset(trie[no], 0, sizeof(trie[no]));
+    fail[no] = 0;
+    isw[no] = 0;
+    return no;
+}
+void insert(char *s, int id) {
+    int len = strlen(s), rt = 0;
+    rep(i, 0, len) {
+        int id = s[i];    // 视情况而定
+        if(!trie[rt][id]) trie[rt][id] = newnode();
+        rt = trie[rt][id];
     }
-    void insert(char *s, int id) {
-        int len = strlen(s), rt = 0;
-        rep(i, 0, len) {
-            int id = s[i];    // 视情况而定
-            if(!trie[rt][id]) trie[rt][id] = newnode();
-            rt = trie[rt][id];
-        }
-        isw[rt] = id;       // 记录单词 id
-    }
-    void build() {  // 构建失配指针
-        que[0] = 0;
-        for (int head = 0, tail = 1; head < tail; ++head) {
-            int cur = que[head];
-            rep(i, 0, 128) {
-                int u = trie[cur][i];
-                if (u) {
-                    int p = fail[cur];
-                    while (p && !trie[p][i]) p = fail[p];
-                    fail[u] = !cur ? 0 : trie[p][i];
-                    last[u] = !cur ? 0 : isw[fail[u]] ? fail[u] : last[fail[u]];
-                    que[tail++] = u;
-                } else {
-                    trie[cur][i] = trie[fail[cur]][i];
-                }
+    isw[rt] = id;       // 记录单词 id
+}
+void build() {  // 构建失配指针
+    que[0] = 0;
+    for (int head = 0, tail = 1; head < tail; ++head) {
+        int cur = que[head];
+        rep(i, 0, 26) {  // 注意范围
+            int u = trie[cur][i];
+            if (u) {
+                int p = fail[cur];
+                while (p && !trie[p][i]) p = fail[p];
+                fail[u] = !cur ? 0 : trie[p][i];
+                last[u] = !cur ? 0 : isw[fail[u]] ? fail[u] : last[fail[u]];
+                que[tail++] = u;
+            } else {
+                trie[cur][i] = trie[fail[cur]][i];
             }
         }
     }
-    void run(char *s) {     // 匹配单词时，可以把 fail 换成 last ，加快匹配
-        int len = strlen(s), rt = 0;
-        rep(i, 0, len) {
-            int id = s[i];
-            while (rt && !trie[rt][id]) rt = fail[rt];
-            int tmp = rt = trie[rt][id];
-            while (tmp) {
-                if (isw[tmp]) cnt[isw[tmp]]++;      // 统计单词出现次数
-                tmp = fail[tmp];
-            }
+}
+void run(char *s) {
+    int len = strlen(s), rt = 0;
+    rep(i, 0, len) {
+        int id = s[i];
+        while (rt && !trie[rt][id]) rt = last[rt];
+        int tmp = rt = trie[rt][id];
+        while (tmp) {
+            if (isw[tmp]) cnt[isw[tmp]]++;      // 统计单词出现次数
+            tmp = last[tmp];
         }
     }
-};
+}
 
 ================================================== Problem Set ==================================================
 
