f2bffb407a151e44125acdbb1e5f4e76b35dc5cd
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-02-07 23:52:48 +0800

    Update Horner Algorithm.cpp

--- template/math/Horner Algorithm.cpp
+++ template/math/Horner Algorithm.cpp
@@ -1,7 +1,6 @@
 // 秦九韶算法
 T Horner(T x) {
     T res = 0;
-    rep(i, 0, n+1)
-        res = res * x + a[i]; // a[i] 为第 i 次项系数
+    per(i, 0, n+1) res = res * x + a[i]; // a[i] 为第 i 次项系数
     return res;
 }
