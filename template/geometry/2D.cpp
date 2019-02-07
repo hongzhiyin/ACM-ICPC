14ba1cc9f2f79bc4b63e3b365fa92f1faddaeebb
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-02-07 19:12:54 +0800

    Update 2D.cpp

--- template/geometry/2D.cpp
+++ template/geometry/2D.cpp
@@ -3,12 +3,12 @@ const db eps = 1e-7, pi = acosl(-1.);
 int sgn(T x) { return (x > eps) - (x < -eps); }
 struct P {
     T x, y; P () {} P (T x, T y) : x(x), y(y) {}
-    P operator - (P b) { return P(x - b.x, y - b.y); }
-    P operator + (P b) { return P(x + b.x, y + b.y); }
-    T operator * (P b) { return x * b.x + y * b.y; }
-    T operator / (P b) { return x * b.y - y * b.x; }
-    P operator * (T k) { return P(x * k, y * k); }
-    P operator / (T k) { return P(x / k, y / k); }
+    P operator + (P b) { return P(x + b.x, y + b.y); }  // 向量加
+    P operator - (P b) { return P(x - b.x, y - b.y); }  // 向量减
+    T operator * (P b) { return x * b.x + y * b.y; }    // 向量点积
+    T operator / (P b) { return x * b.y - y * b.x; }    // 向量叉积
+    P operator * (T k) { return P(x * k, y * k); }      // 向量数乘
+    P operator / (T k) { return P(x / k, y / k); }      // 向量数除
 };
 T cross(P o, P a, P b) { return (a - o) / (b - o); }
 int Scross(P o, P a, P b) { return sgn(cross(o, a, b)); }
@@ -32,7 +32,7 @@ struct C {
     P o; db r; C () {} C (P o, db r) : o(o), r(r) {}
 };
 
-C getC(P a,P b,P c){
+C getC(P a,P b,P c){  // 三点确定一个圆 （ 三角形外接圆 ）
     db a1 = b.x - a.x, b1 = b.y - a.y, c1 = (a1 * a1 + b1 * b1) / 2;
     db a2 = c.x - a.x, b2 = c.y - a.y, c2 = (a2 * a2 + b2 * b2) / 2;
     db d = a1 * b2 - a2 * b1;
