d1c74702660fd8abd58170137d653d3994af3753
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-02-25 23:14:09 +0800

    Update Extend.cpp

--- template/base/Extend.cpp
+++ template/base/Extend.cpp
@@ -35,6 +35,9 @@ inline bool eq(db a, db b) { return fabs(a - b) < eps; }
 inline bool ls(db a, db b) { return a + eps < b; }
 inline bool le(db a, db b) { return eq(a, b) || ls(a, b); }
 
+// 大随机数
+ll myrand() { return (ll)rand() << 32 ^ (ll)rand() << 16 ^ rand(); }
+
 // GCC 内建函数
 int __builtin_ffs (unsigned int x)      // 返回右起第一个 1 的位置，从 1 开始，如果 x = 0 ，返回 0 。
 int __builtin_clz (unsigned int x)      // 返回前导 0 的个数，如果 x = 0 ，结果未定义。
