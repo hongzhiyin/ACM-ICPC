ab19b47cb538267c96a2af8073bfe973476313e7
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-02-27 15:24:31 +0800

    Update Treap.cpp

--- template/data structure/Treap.cpp
+++ template/data structure/Treap.cpp
@@ -4,17 +4,12 @@
 1. 虽然结构与线段树类似，但关键的区别在于，对于非叶子结点，在线段树中代表区间，在平衡树中代表一个独立的值。
 2. 一棵树，代表一个区间，即一个长条数组，而树的根节点，是这个区间中的一个元素，左子树表示其左端区间，右子树同理，这是与线段树的本质区别。
 3. 分裂就是把一个长条分成两个长条，合并就是把两个长条按拼在一起。
+
 */
 
-// 非旋转可持久化 Treap （待填坑）
-
-// https://www.cnblogs.com/nbwzyzngyl/p/7977369.html
-
-// 非旋转 Treap （功能待完善）
-// https://www.cnblogs.com/DriverLao/p/8087423.html
-
 // 针对序列操作
-// 二叉搜索树按序列排序，左子树结点表示在序列中位置在根结点前面，右子树反之。
+// 二叉搜索树按 !! 序列位置 !! 排序，左子树结点表示在序列中位置在根结点前面，右子树反之。
+
 #define lson t[rt].l
 #define rson t[rt].r
 int rt, no;
@@ -115,6 +110,93 @@ int Qry(int l, int r, int op) {  // 区间查询 ( 1. 最小值 ; 2. 区间和 )
 vi vout;
 void out(int rt) {
     PushDown(rt);
+    if (lson) out(lson);
+    vout.pb(t[rt].val);
+    if (rson) out(rson);
+}
+void print(int rt) {  // 输出 rt 对应区间
+    vout.clear();
+    out(rt);
+    rep(i, 0, sz(vout)) printf("%d%c", vout[i], " \n"[i==sz(vout)-1]);
+}
+
+----------------------------------------------------------------------------------------------------
+
+// 针对有序可重集合操作
+// 二叉搜索树按 !! 元素值 !! 排序，左子树结点表示所有元素值比根结点元素值小的结点，右子树反之。
+
+#define lson t[rt].l
+#define rson t[rt].r
+int rt, no;
+struct Node { int l, r, val, rnd, sz; } t[N];
+void Clear() {
+    rt = no = 0;
+    t[0] = (Node){ 0, 0, 0, rand(), 0 };
+}
+int newnode(int val) { t[++no] = (Node){ 0, 0, val, rand(), 1 }; return no; }
+void PushUp(int rt) { t[rt].sz = t[lson].sz + t[rson].sz + 1; }
+void split(int rt, int k, int &x, int &y) {  // 将 rt 对应的一段区间分成元素值 小于等于 k 和 大于 k 的两段，根结点分别为 x 和 y
+	if (!rt) { x = y = 0; return ; }
+	if (t[rt].val <= k) { x = rt; split(rson, k, rson, y); }
+	else { y = rt; split(lson, k, x, lson); }
+	PushUp(rt);
+}
+int merge(int x, int y) {  // 将 x 和 y 对应的区间按 x 区间在左， y 区间在右的顺序合并起来，返回合并后的根结点
+    if (!x || !y) return x + y;
+    if (t[x].rnd < t[y].rnd) {
+        t[x].r = merge(t[x].r, y);
+        PushUp(x); return x;
+    } else {
+        t[y].l = merge(x, t[y].l);
+        PushUp(y); return y;
+    }
+}
+void INS(int val) {  // 在 最后一个 值小于等于 val 的元素后面插入 值为 val 的元素
+    int x, y;
+    split(rt, val, x, y);
+    rt = merge(merge(x, newnode(val)), y);
+}
+void DEL(int val) {  // 删除 随机一个 值等于 val 的元素，如果不存在则没有影响
+    int x, y, z;
+    split(rt, val-1, x, y); split(y, val, y, z);
+    rt = merge(merge(x, merge(t[y].l, t[y].r)), z);
+}
+void Build(int a[], int n) {  // 初始序列 a[] ，从 1 开始
+    rep(i, 1, n+1) INS(a[i]);
+}
+int RANK(int val) {  // 查询值为 val 的元素的排名 ( 排名定义为 值比 val 小的元素 的个数 + 1 , 即最前面的 值为 val 的元素 的位置 )
+	int x, y, rank;
+	split(rt, val-1, x, y);
+	rank = t[x].sz + 1;
+	rt = merge(x, y);
+	return rank;
+}
+int VAL(int rt, int rank) {  // 查询 rt 对应区间内排名为 rank 的元素的值 val ( rank 相当于位置，具有相同值的元素也有不同的位置 )
+	for (;;) {
+		if (rank == t[lson].sz + 1) return t[rt].val;
+		if (rank <= t[lson].sz) rt = lson;
+		else { rank -= t[lson].sz + 1; rt = rson; }
+	}
+}
+int PRE(int val) {  // 返回小于 val 且最大的元素值
+	int x, y, rank, pre;
+	split(rt, val-1, x, y);
+	if (!x) return -INF;  // 若不存在，则返回负无穷
+	rank = t[x].sz;
+	pre = VAL(x, rank);
+	rt = merge(x, y);
+	return pre;
+}
+int NET(int val) {  // 返回大于 val 且最小的元素值
+	int x, y, net;
+	split(rt, val, x, y);
+	if (!y) return INF;  // 若不存在，则返回正无穷
+	net = VAL(y, 1);
+	rt = merge(x, y);
+	return net;
+}
+vi vout;
+void out(int rt) {
     if (lson) out(lson);
     vout.pb(t[rt].val);
     if (rson) out(rson);
