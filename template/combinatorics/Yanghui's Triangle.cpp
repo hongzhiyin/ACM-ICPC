ea86e97cc3986bbfbd6d207f457f9c215a1e59ea
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-06-07 17:14:21 +0800

    Update Yanghui's Triangle.cpp

--- template/combinatorics/Yanghui's Triangle.cpp
+++ template/combinatorics/Yanghui's Triangle.cpp
@@ -12,7 +12,7 @@ int C(int n, int m) {
     return res;
 }
 
-//求前 n 行
+// 求前 n 行
 ll C[N][N];
 void Yanghui() {
     memset(C, 0, sizeof(C));
@@ -22,11 +22,20 @@ void Yanghui() {
     }
 }
 
-//求第 n 行（不取模）
+// 求第 n 行（不取模）
 C[0] = 1;
 rep(i, 1, n+1) C[i] = C[i-1] * (n - i + 1) / i;
 
-//求第 n 行（取模）
+// 求第 n 行（取模）
 inv[1] = C[0] = 1;
 rep(i, 2, n+1) inv[i] = mul(inv[MOD%i], MOD - MOD / i);
 rep(i, 1, n+1) C[i] = mul(mul(C[i-1], n - i + 1), inv[i]);
+
+// 求第 n 列（不取模）
+C[n] = 1;
+rep(i, n+1, N) C[i] = C[i-1] * i / (i - n);
+
+// 求第 n 列（取模）
+C[n] = inv[1] = 1;
+rep(i, 2, N) inv[i] = mul(inv[MOD%i], MOD - MOD / i);
+rep(i, n+1, N) C[i] = mul(mul(C[i-1], i), inv[i-n]);
