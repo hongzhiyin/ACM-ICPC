fad79143bac3bd519ccdae250cc0fa2c5a9a8fb8
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-06-19 19:11:44 +0800

    Create BCC.cpp

---
+++ template/graph/BCC.cpp
@@ -0,0 +1,33 @@
+bool bri[N];
+vector <pii> e[N];
+int dfn[N], low[N], bcc[N], S[N], top, cnt, no;
+void BCC(int u, int eid) {
+    dfn[u] = low[u] = ++no;
+    S[top++] = u;
+    for (auto o : e[u]) {
+        int v = o.fi, id = o.se;
+        if (!dfn[v]) {
+            BCC(v, id);
+            low[u] = min(low[u], low[v]);
+            if (low[v] > dfn[u]) bri[id] = 1;
+        } else if (id != eid) {
+            low[u] = min(low[u], dfn[v]);
+        }
+    }
+    if (low[u] == dfn[u]) {
+        ++cnt;
+        do { bcc[S[--top]] = cnt; } while (S[top] != u);
+    }
+}
+
+vi e2[N];
+void run(int n) {
+    no = top = cnt = 0;
+    memset(dfn, 0, sizeof(dfn));
+    memset(bri, 0, sizeof(bri));
+    memset(bcc, 0, sizeof(bcc));
+    rep(i, 1, n+1) if (!dfn[i]) BCC(i, 0);
+    rep(i, 1, cnt+1) e2[i].clear();
+    rep(u, 1, n+1) for (auto o : e[u])
+        if (bcc[u] != bcc[o.fi]) e2[bcc[u]].pb(bcc[o.fi]);
+}
