699a2a910d91ce86aea33bbf6914dea8562d9fcf
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-07-10 21:26:19 +0800

    Update Euler Path.cpp

--- template/graph/Euler Path.cpp
+++ template/graph/Euler Path.cpp
@@ -1,11 +1,37 @@
-void dfs(int u) {
-    for (auto &v : e[u]) if (v.se) {
-        v.se = 0;
-        dfs(v.fi);
-        ans.pb(mp(v.fi, u));
+int eno, to[N], net[N], head[N];
+void AddEdge(int u, int v) {
+    to[++eno] = v; net[eno] = head[u]; head[u] = eno;
+}
+
+vi path;
+bool vis[N];
+int top, sta[N];
+void EulerPath(int st) {
+    sta[++top] = st;
+    while (top) {
+        int u = sta[top], i = head[u];
+        while (i && vis[i]) i = net[i];
+        if (i) {
+            sta[++top] = to[i];
+            vis[i] = vis[i ^ 1] = 1;  // 标记当前这条无向边
+            head[u] = net[i];  // 修改表头，删除已访问过的边
+        } else {
+            top--;
+            path.pb(u);
+        }
     }
 }
-void Solve() {  // 输出从 start 出发的一条欧拉路径（回路），根据度数定义找到正确的起始点
-    dfs(start);
-    for (auto x : ans) printf("%d %d\n", x.fi, x.se);
+
+void Init() {
+    eno = 1;  // 初始化边编号，方便存储正反边
+    rep(i, 0, m) {
+        int u, v; scanf("%d%d", &u, &v);
+        AddEdge(u, v); AddEdge(v, u);
+    }
+}
+
+int Solve() {
+    EulerPath(1);  // 以 1 为起点
+    per(i, 0, sz(path)) printf("%d\n", path[i]);  // 路径需逆序输出
+    return 0;
 }
