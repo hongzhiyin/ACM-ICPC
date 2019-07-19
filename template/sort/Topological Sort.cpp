d0c4f0a48a668f3c7d11017058422dded8f89e6c
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-07-19 14:57:21 +0800

    Update Topological Sort.cpp

--- template/sort/Topological Sort.cpp
+++ template/sort/Topological Sort.cpp
@@ -1,3 +1,16 @@
+int no, in[N], top[N];
+void dfs(int u) {
+    top[u] = ++no;
+    for (auto v : e[u]) if (!--in[v]) dfs(v);
+}
+
+void Init() {
+    no = 0;
+    memset(in, 0, sizeof(in));
+    memset(top, 0, sizeof(top));
+    rep(i, 1, n+1) if (!in[i] && !top[i]) dfs(i);
+}
+
 ================================================== Problem Set ==================================================
 
 // cf 1037E
