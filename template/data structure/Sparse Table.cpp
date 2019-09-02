b54962c01721e1a7143897b1f152802e3bbf06eb
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-02 21:11:09 +0800

    Update Sparse Table.cpp

--- template/data structure/Sparse Table.cpp
+++ template/data structure/Sparse Table.cpp
@@ -1,8 +1,4 @@
 /*
-【函数功能】
-void st_init(T a[], int n) : 以长度为 n 的数组 a[] 初始化 ST 表
-T Qry(int L, int R)     : 询问区间 [L, R] 的最值
-
 【注意事项】
 1. a[] 从 1 开始
 2. 根据需要修改 min or max
@@ -10,14 +6,15 @@ T Qry(int L, int R)     : 询问区间 [L, R] 的最值
 */
 
 int lg[N]; T d[N][21];
-void st_init(int a[], int n) {
-    rep(i, 1, n+1) d[i][0] = a[i];
-    rep(j, 1, 21) for (int i = 1; i + (1 << j) - 1 <= n; ++i)
-        d[i][j] = op(d[i][j-1], d[i + (1 << (j-1) )][j-1]);
-    lg[1] = 0; rep(i, 2, n+1) lg[i] = lg[i>>1] + 1;
-}
-
-T Qry(int l, int r) {
-    int k = lg[r-l+1];
-    return op(d[l][k], d[r-(1<<k)+1][k]);
-}
+struct ST {
+    void init(T a[], int n) {
+        rep(i, 1, n+1) d[i][0] = a[i];
+        rep(j, 1, 21) for (int i = 1; i + (1 << j) - 1 <= n; ++i)
+            d[i][j] = op(d[i][j-1], d[i + (1 << (j-1) )][j-1]);
+        lg[1] = 0; rep(i, 2, n+1) lg[i] = lg[i>>1] + 1;
+    }
+    T qry(int l, int r) {
+        int k = lg[r-l+1];
+        return op(d[l][k], d[r-(1<<k)+1][k]);
+    }
+} rmq;
