f67d46b905236fd7412a41cdff469ebb8ca76b96
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-03 15:55:32 +0800

    Update @ vimrc.cpp

--- template/base/@ vimrc.cpp
+++ template/base/@ vimrc.cpp
@@ -1,8 +1,8 @@
-set nu ai ci si mouse=a ts=4 sts=4 sw=4 fdm=marker
-nmap<F2> : vs %<.in <CR>                                          // 新建输入文件
-nmap<F3> : !gedit % <CR>                                          // 用 gedit 打开
-nmap<F5> : !./%< <CR>                                             // 在命令行中运行
-nmap<F8> : !time ./%< < %<.in <CR>                                // 文件运行，显示运行时间
-nmap<F9> : :w <CR> :make %< <CR>                                  // 保存并编译
-nmap<F10> : :w <CR> :!g++ % -o %< -O2 -g -std=c++11 -Wall <CR>    // 保存并编译（优化）
-nmap<F12> ggvGd:r head.cpp <CR> kdd
+se nu si mouse=a ts=4 sts=4 sw=4 fdm=marker et
+nm<F2> : vs %<.in <CR>                                          // 新建输入文件
+nm<F3> : !gedit % <CR>                                          // 用 gedit 打开
+nm<F5> : !./%< <CR>                                             // 在命令行中运行
+nm<F8> : !time ./%< < %<.in <CR>                                // 文件运行，显示运行时间
+nm<F9> : :w <CR> :make %< <CR>                                  // 保存并编译
+nm<F10> : :w <CR> :!g++ % -o %< -O2 -g -std=c++11 -Wall <CR>    // 保存并编译（优化）
+nm<F12> ggvGd:r head.cpp <CR> kdd
