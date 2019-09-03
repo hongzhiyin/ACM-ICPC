5f9d15755b991d487fb97a5f4169dcb5000bc78e
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-03 15:57:28 +0800

    Update @ vimrc.cpp

--- template/base/@ vimrc.cpp
+++ template/base/@ vimrc.cpp
@@ -4,5 +4,5 @@ nm<F3> : !gedit % <CR>                                          // 用 gedit 打
 nm<F5> : !./%< <CR>                                             // 在命令行中运行
 nm<F8> : !time ./%< < %<.in <CR>                                // 文件运行，显示运行时间
 nm<F9> : :w <CR> :make %< <CR>                                  // 保存并编译
-nm<F10> : :w <CR> :!g++ % -o %< -O2 -g -std=c++11 -Wall <CR>    // 保存并编译（优化）
+nm<F7> : :w <CR> :!g++ % -o %< -O2 -g -std=c++11 -Wall <CR>     // 保存并编译（优化）
 nm<F12> ggvGd:r head.cpp <CR> kdd
