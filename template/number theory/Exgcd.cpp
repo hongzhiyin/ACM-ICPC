c6cfb569227dad518ccfbeaf871cb7a54f3022f7
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-08 20:47:06 +0800

    Update Exgcd.cpp

--- template/number theory/Exgcd.cpp
+++ template/number theory/Exgcd.cpp
@@ -1,6 +1,8 @@
 // ax + by = gcd(a, b)
 
-void exgcd(ll a, ll b, ll &g, ll &x, ll &y) {
-    if (!b) { g = a; x = 1; y = 0; }
-    else { exgcd(b, a % b, g, y, x); y -= x * (a / b); }
+ll exgcd(ll a, ll b, ll &x, ll &y) {
+    if (!b) { x = 1; y = 0; return a; }
+    ll g = exgcd(b, a % b, y, x);
+    y -= x * (a / b);
+    return g;
 }
