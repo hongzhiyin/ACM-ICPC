ad749750378031d5afd0a58da2af752fb3e8a085
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-30 19:02:07 +0800

    Update Extend KMP.cpp

--- template/string/Extend KMP.cpp
+++ template/string/Extend KMP.cpp
@@ -1,38 +1,42 @@
-// 讲解：https://blog.csdn.net/dyx404514/article/details/41831947
-// 代码：https://subetter.com/articles/2018/04/extended-kmp-algorithm.html
+/*
+< 准备 >
+    1. 字符串 s[] 与字符串 t[] ，下标从 0 开始
 
-int net[N], ex[N];  // ex[i] 表示 T 与 S[i, n-1] 的最长公共前缀
-char s[N], t[N];
-struct ExKMP {  // 求 T 与 S 的每一个后缀的最长公共前缀
-    void Get(char *t, int m) {
+< 使用 >
+    1. 调用 ex.exkmp(s, n, t, m) ， n 和 m 分别为 s[] 与 t[] 的长度，得
+    .. lcp[i] 表示 t[] 与 s[i, n-1] 的最长公共前缀长度
+    .. net[i] 表示 t[] 与 t[i, m-1] 的最长公共前缀长度
+*/
+
+int net[N], lcp[N];
+struct Extend_KMP {
+    void Get(char t[], int m) {
         int a = 0, p = 0;
         net[0] = m;
         rep(i, 1, m) {
             if (i >= p || i + net[i-a] >= p) {
                 if (i >= p) p = i;
                 while (p < m && t[p] == t[p-i]) p++;
-                net[i] = p - i;
-                a = i;
+                net[a=i] = p - i;
             } else {
                 net[i] = net[i-a];
             }
         }
     }
-    void exkmp(char *s, int n, char *t, int m) {
+    void exkmp(char s[], int n, char t[], int m) {
         Get(t, m);
         int a = 0, p = 0;
         rep(i, 0, n) {
             if (i >= p || i + net[i-a] >= p) {
                 if (i >= p) p = i;
                 while (p < n && p - i < m && s[p] == t[p-i]) p++;
-                ex[i] = p - i;
-                a = i;
+                lcp[a=i] = p - i;
             } else {
-                ex[i] = net[i-a];
+                lcp[i] = net[i-a];
             }
         }
     }
-};
+} ex;
 
 ================================================== Problem Set ==================================================
 
