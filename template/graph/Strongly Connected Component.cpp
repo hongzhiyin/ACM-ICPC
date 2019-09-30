43797cefac1056a67db80df0eec21e6c3aa05873
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-09-30 18:08:28 +0800

    Update and rename Tarjan.cpp to Strongly Connected Component.cpp

---
+++ template/graph/Strongly Connected Component.cpp
@@ -0,0 +1,46 @@
+/*
+< 准备 >
+    1. 有向图边集 vector<int> e[]
+
+< 使用 >
+    1. 调用 Scc.run(n) ， n 为有向图顶点数，得
+    .. dfn[] : 顶点的 dfs 序
+    .. low[] : 追溯值，即顶点能到达的最小 dfs 序
+    .. scc[] : 顶点所属强连通分量的编号
+    .. cnt : 强连通分量个数
+    2. 调用 Scc.run(n) 后即完成了缩点，得
+    .. 缩点后新图的边集 vector<int> e2[]
+
+< 注意 >
+    1. 缩点建新图的过程中，可增加统计其他所需的信息，如入度、出度等。
+*/
+
+vi e2[N];
+int dfn[N], low[N], scc[N], S[N], top, cnt, no;
+struct Strongly_Connected_Component {
+    void SCC(int u) {
+        dfn[u] = low[u] = ++no;
+        S[top++] = u;
+        for (auto v : e[u]) {
+            if (!dfn[v]) {
+                SCC(v);
+                low[u] = min(low[u], low[v]); 
+            } else if (!scc[v]) {
+                low[u] = min(low[u], dfn[v]);
+            }
+        }
+        if (low[u] == dfn[u]) {
+            cnt++;
+            do { scc[S[--top]] = cnt; } while (S[top] != u);
+        }
+    }
+    void run(int n) {
+        no = top = cnt = 0;
+        memset(dfn, 0, sizeof(dfn[0]) * (n+1));
+        memset(scc, 0, sizeof(scc[0]) * (n+1));
+        rep(i, 1, n+1) if (!dfn[i]) SCC(i);
+        rep(i, 1, cnt+1) e2[i].clear();                    // 缩点前初始化
+        rep(u, 1, n+1) for (auto v : e[u])                 // 缩点部分
+            if (scc[u] != scc[v]) e2[scc[u]].pb(scc[v]);
+    }
+} Scc;
