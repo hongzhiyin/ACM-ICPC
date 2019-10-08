6662805fc141109a63fa5457a27bbcd28409e828
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-10-08 13:50:00 +0800

    Create generate data.cpp

---
+++ template/zone/generate data.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef double db;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+#define dd(x) cout << #x << " = " << x << ", "
+#define de(x) cout << #x << " = " << x << endl
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
+#define per(i,a,b) for(int i=(b)-1;i>=(a);--i)
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+const int N = -1;
+
+// ------- 变量 ------- //
+
+
+
+// ------- 函数 ------- //
+
+void Init() {
+    
+}
+
+int Solve() {
+
+}
+
+// ------- main ------- //
+
+int main ()
+{
+    char in[] = "x.in", out[] = "x.out";
+    int t = 10; while (t--) {
+        in[0] = out[0] = t + '0';
+        
+        // generate x.in
+        freopen(in, "w", stdout);
+        
+        // read x.in , generate x.out
+        freopen(in, "r", stdin);
+        freopen(out, "w", stdout);
+        
+        Init();
+        Solve();
+    }
+    return 0;
+}
