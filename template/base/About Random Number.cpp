14e69169d40bab1b01de97fcbecb1f5392e36e7a
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-10-08 13:40:01 +0800

    Update About Random Number.cpp

--- template/base/About Random Number.cpp
+++ template/base/About Random Number.cpp
@@ -5,15 +5,16 @@
     uniform_real_distribution : 浮点数 [l, r) 均匀分布
 
 < 使用 >
-    1. rnd_ll(l, r) 返回 [l, r] 之间的一个整型随机数
-    2. rnd_db(l, r) 返回 [l, r) 之间的一个浮点随机数
-    3. shuffle(a.begin(), a.end(), rng); 可将容器 a 随机混洗
+    1. rll(l, r)  : 返回 [l, r] 之间的一个整型随机数
+    2. rdb(l, r)  : 返回 [l, r) 之间的一个浮点随机数
+    3. shuf(a)    : 将容器 a 随机混洗
+    4. shuf(a, n) : 将长度为 n 的数组 a[] 随机混洗，下标从 0 开始
 */
 
 mt19937 rng(time(0));
-ll rnd_ll(ll l, ll r) {
-    return uniform_int_distribution <ll> (l, r) (rng);
-}
-db rnd_db(db l, db r) {
-    return uniform_real_distribution <db> (l, r) (rng);
-}
+struct Random_Number_Generator {
+    ll rll(ll l, ll r) { return uniform_int_distribution <ll> (l, r) (rng); }
+    db rdb(db l, db r) { return uniform_real_distribution <db> (l, r) (rng); }
+    template <typename T> void shuf(vector<T> &a) { shuffle(a.begin(), a.end(), rng); } 
+    template <typename T> void shuf(T a[], int n) { shuffle(a, a + n, rng); }
+} rnd;
