633c04d7dd18934d4e3c6de41ec20f227cba7f32
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-10-15 20:03:55 +0800

    Update About Random Number.cpp

--- template/base/About Random Number.cpp
+++ template/base/About Random Number.cpp
@@ -5,10 +5,12 @@
     uniform_real_distribution : 浮点数 [l, r) 均匀分布
 
 < 使用 >
-    1. rll(l, r)  : 返回 [l, r] 之间的一个整型随机数
-    2. rdb(l, r)  : 返回 [l, r) 之间的一个浮点随机数
-    3. shuf(a)    : 将容器 a 随机混洗
-    4. shuf(a, n) : 将长度为 n 的数组 a[] 随机混洗，下标从 0 开始
+    1. rll(l, r)     : 返回 [l, r] 之间的一个整型随机数
+    2. rdb(l, r)     : 返回 [l, r) 之间的一个浮点随机数
+    3. shuf(a)       : 将容器 a 随机混洗
+    4. shuf(a, n)    : 将长度为 n 的数组 a[] 随机混洗，下标从 0 开始
+    5. shuf(a, l, r) : 将数组 a[] 中下标从 l 到 r 的部分随机混洗
+    6. rtree(n, e)   : 随机生成一棵 n 个节点的树，根节点为 1 ，边集保存在 vi e[] 中
 */
 
 mt19937 rng(time(0));
@@ -17,4 +19,6 @@ struct Random_Number_Generator {
     db rdb(db l, db r) { return uniform_real_distribution <db> (l, r) (rng); }
     template <typename T> void shuf(vector<T> &a) { shuffle(a.begin(), a.end(), rng); } 
     template <typename T> void shuf(T a[], int n) { shuffle(a, a + n, rng); }
+    template <typename T> void shuf(T a[], int l, int r) { shuffle(a + l, a + r + 1, rng); }
+    void rtree(int n, vi e[]) { rep(i, 0, n+1) { e[i].clear(); if (i > 1) e[rll(1, i-1)].pb(i); } }
 } rnd;
