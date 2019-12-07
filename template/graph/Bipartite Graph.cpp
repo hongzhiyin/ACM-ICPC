8f33dfabd04679f619f3081dbe52bd2dd790d599
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2019-12-07 17:59:17 +0800

    Update Bipartite Graph.cpp

--- template/graph/Bipartite Graph.cpp
+++ template/graph/Bipartite Graph.cpp
@@ -42,48 +42,47 @@ int hungary() {   // 返回最大匹配数
 // https://blog.csdn.net/c20180630/article/details/71080521
 // https://blog.csdn.net/u014097230/article/details/51554905
 
+// init!! , id starts from 0
+// n <= m
+
+template<class T>
 struct KM {
 	static const int N = 505;
-	static const int inf = ~0U>>2;
-	int n, m, l[maxn], pre[maxn], used[maxn];
-	int g[maxn][maxn], lx[maxn], ly[maxn], slack[maxn];
-	void ini(int _n,int _m){
+	static const T inf = ~0U>>2;
+	int n, m, left[N], pre[N], used[N];
+	T g[N][N], Lx[N], Ly[N], slack[N];
+	void ini(int _n, int _m) {
 		n = _n, m = _m;
-		rep(i,0,n) rep(j,0,m) g[i][j] = G[i][j];
-		//rep(i,0,n) g[i][i] = -inf;
-	/*	rep(i,0,n){
-			rep(j,0,m) dd(g[i][j]);
-			printf("\n");
-		}*/
+		rep(i, 0, n) rep(j, 0, m) g[i][j] = -inf;
 	}
-	void go(int now){
-		rep(i,0,m+1) used[i] =0 , slack[i] = inf;
-		l[m] = now;
+	void go(int now) {
+		rep(i, 0, m+1) used[i] = 0, slack[i] = inf;
+		left[m] = now;
 		int u, v;
-		for(u = m; ~l[u];u = v){
-			used[u] =1;
-			int d = inf;
-			rep(i,0,m) if(!used[i]){
-				int tmp = lx[l[u]] + ly[i] - g[l[u]][i];
-				if(tmp < slack[i]) slack[i] = tmp, pre[i] = u;
-				if(slack[i] < d)  d= slack[ v = i];
+		for(u = m; ~left[u]; u = v) {
+			used[u] = 1;
+			T d = inf;
+			rep(i, 0, m) if(!used[i]) {
+				T tmp = Lx[left[u]] + Ly[i] - g[left[u]][i];
+				if (tmp < slack[i]) slack[i] = tmp, pre[i] = u;
+				if (slack[i] < d) d = slack[v = i];
 			}
-			rep(i,0,m+1) if(used[i]) lx[l[i]] -= d, ly[i] += d;
+			rep(i, 0, m+1) if (used[i]) Lx[left[i]] -= d, Ly[i] += d;
 			else slack[i] -= d;
 		}
-		for(; u!=m; l[u]=l[pre[u]], u = pre[u]);
+		for(; u != m; left[u] = left[pre[u]], u = pre[u]);
 	}
-	int run(){
-		fill_n(lx,n,0);
-		fill_n(ly,m,0);
-		fill_n(l,m,-1);
-		rep(i,0,n) go(i);
-		int ans =0;
-		rep(i,0,n) ans+=lx[i];
-		rep(i,0,m) ans+=ly[i];
+	T run() {
+		fill_n(Lx, n, 0);
+		fill_n(Ly, m, 0);
+		fill_n(left, m, -1);
+		rep(i, 0, n) go(i);
+		T ans = 0;
+		rep(i, 0, n) ans += Lx[i];
+		rep(i, 0, m) ans += Ly[i];
 		return ans;
 	}
-}T;
+};
 
 ------------------------------------------------------------------------------------------------
 
