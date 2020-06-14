1969bafe074f0c7cf957f1cfcd14b5a191442ae9
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-06-15 01:30:40 +0800

    Update Quick Sort.cpp

--- template/sort/Quick Sort.cpp
+++ template/sort/Quick Sort.cpp
@@ -1,12 +1,21 @@
-void qsort(int a[], int l, int r) {
-    if (l >= r) return ;
-    int i = l, j = r;
-    ll mid = a[(l+r)>>1];
+/* ----- 快速排序 -----
+< 参数 >
+    1. first 和 last 为随机访问迭代器
+
+< 使用 >
+    1. 使用方法同 std::sort()
+*/
+
+template <class Iter>
+void Quick_Sort(Iter first, Iter last) {
+    if (first == last) return;
+    Iter i = first, j = last - 1;
+    typename iterator_traits<Iter>::value_type pivot = *(i + (j - i) / 2);
     while (i <= j) {
-        while (a[i] < mid) ++i;
-        while (a[j] > mid) --j;
-        if (i <= j) swap(a[i++], a[j--]);
+        while (*i < pivot) ++i;
+        while (*j > pivot) --j;
+        if (i <= j) swap(*(i++), *(j--));
     }
-    qsort(a, l, j);
-    qsort(a, i, r);
+    Quick_Sort(first, j + 1);
+    Quick_Sort(i, last);
 }
