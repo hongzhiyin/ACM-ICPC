a37fcff41ce7ca567afef1d9c1de3ce9d75f5fd9
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-06-20 20:02:05 +0800

    upd

--- template/base/@ Header.cpp
+++ template/base/@ Header.cpp
@@ -1,25 +1,5 @@
-#pragma region header
 #include <bits/stdc++.h>
 using namespace std;
-typedef double db;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
-#define dd(x) cout << #x << " = " << x << ", "
-#define de(x) cout << #x << " = " << x << endl
-#define rep(i,a,b) for(int i=(a);i<(b);++i)
-#define per(i,a,b) for(int i=(b)-1;i>=(a);--i)
-#define all(x) (x).begin(),(x).end()
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-//INF = 1 061 109 567 ; INF + INF = 2 122 219 134 ;
-//LINF = 4 557 430 888 798 830 399 ; LINF + LINF = 9 114 861 777 597 660 798 ;
-const int INF = 0x3f3f3f3f;          // -INF: 0xc0   abs(-INF) = INF + 1
-const ll LINF = 0x3f3f3f3f3f3f3f3f;
-#pragma endregion
 
 const int N = -1;                    // (1 << [17, 18, 20, 21] ) > [1e5, 2e5, 1e6, 2e6]
 
--- template/base/Discrete.cpp
+++ template/base/Discrete.cpp
@@ -1,8 +1,19 @@
-vi dis;
-void Discrete(int a[], int n) {
-    dis.clear();
-    rep(i, 1, n+1) dis.pb(a[i]);
-    sort(all(dis));
-    dis.erase(unique(all(dis)), dis.end());
-    rep(i, 1, n+1) d[i] = lower_bound(all(dis), a[i]) - dis.begin() + 1;
+/* ----- 离散化 -----
+< 参数 >
+    1. first 和 last 为随机访问迭代器
+
+< 使用 >
+    1. 使用方法同 std::sort()
+*/
+
+template <class Iter>
+auto Discrete(Iter first, Iter last) {
+    typedef typename iterator_traits<Iter>::value_type T;
+    vector <T> dis, res(last - first + 1);                                          // 临时容器和待返回容器
+    for (Iter i = first; i != last; ++i) dis.push_back(*i);                         // 放入临时容器
+    sort(dis.begin(), dis.end());                                                   // 排序
+    dis.erase(unique(dis.begin(), dis.end()), dis.end());                           // 去重
+    for (Iter i = first; i != last; ++i)                                            // 二分获取离散值
+        res[i-first+1] = lower_bound(dis.begin(), dis.end(), *i) - dis.begin() + 1; // 下标和离散值都从 1 开始
+    return res;                                                                     // 返回离散后的容器
 }
