7f07221564ab7467763baacccb59997570a5e55f
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-06-24 01:22:26 +0800

    update binary search

--- template/search/Binary Search.cpp
+++ template/search/Binary Search.cpp
@@ -1,31 +1,33 @@
-/*
-    1. 终止条件 L == R
-    2. 求最大值不会取到 L ，求最小值不会取到 R
-    3. [l, r] 表示二分区间， l-1 和 r+1 表示越界下标，如果 L == R 等于越界下标，说明无解
+/* ----- 二分查找 -----
+< 参数 >
+    1. 二分区间边界值 left 与 right
+    2. 判别函数 check
+    3. 可自行添加判别函数的其他参数，记得在代码中修改
+
+< 返回 >
+    1. pair< 是否无解，最终二分值 >
 */
 
 // 求最大值
-int L = l - 1, R = r;
-while (L < R) {
-    int M = L + R + 1 >> 1;
-    if (check(M)) L = M; else R = M - 1;
+template <class T, class Func>
+pair<bool, T> Binary_Search_Max(T left, T right, Func check) {      // 左右边界与判别函数
+    T Left = left - 1, Right = right;                               // 左边界预留越界下标
+    while (Left < Right) {                                          // 终止条件即二者相等
+        T Middle = Left + Right + 1 >> 1;                           // 偶数长度区间的中点落在靠右一侧
+        if (check(Middle)) Left = Middle;                           // 中点可行
+        else Right = Middle - 1;                                    // 中点不可行
+    }
+    return make_pair(Left != left - 1, Left);                       // 是否无解，最终二分值
 }
 
 // 求最小值
-int L = l, R = r + 1;
-while (L < R) {
-    int M = L + R >> 1;
-    if (check(M)) R = M; else L = M + 1;
+template <class T, class Func>
+pair<bool, T> Binary_Search_Min(T left, T right, Func check) {      // 左右边界与判别函数
+    T Left = left, Right = right + 1;                               // 右边界预留越界下标
+    while (Left < Right) {                                          // 终止条件即二者相等
+        T Middle = Left + Right>> 1;                                // 偶数长度区间的中点落在靠左一侧
+        if (check(Middle)) Right = Middle;                          // 中点可行
+        else Left = Middle + 1;                                     // 中点不可行
+    }
+    return make_pair(Left != right + 1, Left);                      // 是否无解，最终二分值
 }
-
-// 浮点数
-db L = l, R = r;
-while (!eq(L, R)) {     // 精度太小可能 TLE ，可以用固定次数的循环
-    db M = (L + R) / 2;
-    if (check(M)) L = M; else R = M;
-}
-printf("%.4f\n", 0.5 * (L + R));
-
-// 各种莫名其妙的精度问题：
-// 保留两位小数，其余位舍去：
-printf("%.2f\n", floor(x * 100)/100);
