73182a9f0d850c3bba917e601fcdf085aeaa1ae1
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-07-25 18:59:33 +0800

    update trie

--- template/string/Trie.cpp
+++ template/string/Trie.cpp
@@ -1,33 +1,24 @@
-// https://www.cnblogs.com/TheRoadToTheGold/p/6290732.html
-// 在单词表中查找单词或前缀
+/* ----- 字典树 ----- */
 
-int no, trie[N * len][k];    // 字母种类 : k ; 单词长度 : len ; 单词个数 : N
-int newnode() {
-    ++no;
-    memset(trie[no], 0, sizeof(trie[no]));
-    // isw[no] = 0;
-    // sum[no] = 0;
-    return no;
-}
-void Clear() { no = -1; newnode(); }    // 使用前注意先用 Clear() 初始化
-void insert(char *s) {     // 插入单词 s
-    int len = strlen(s), rt = 0;   // 根节点编号为 0
-    rep(i, 0, len) {
-        int id = s[i] - 'a';
-        if(!trie[rt][id]) trie[rt][id] = newnode();  // 如果之前没有从 rt 到 id 的前缀，插入新节点
-        rt = trie[rt][id];      // 顺着字典树往下走
-        // sum[rt]++;       // 保存前缀出现次数
+struct Trie {
+    int fork;                                                       // 节点分支数
+    struct node {                                                   // 字典树节点
+        vector<node*> next;                                         // 节点分支
+        bool isword;                                                // 从根节点到当前节点是否是一个完整单词
+        node(int fork) {                                            // 新建一个 fork 个分支的节点
+            next = vector<node*>(fork, nullptr);                    // fork 个分支初始为空指针
+            isword = 0;                                             // 标记位清零
+        }
+    } *root;                                                        // 根节点指针
+    Trie(int fork) : fork(fork) { root = new node(fork); }          // 初始化，建立根节点
+
+    void insert(string &s) {                                        // 插入单词
+        node *cur = root;                                           // 临时指针
+        for (auto ch : s) {                                         // 遍历每个字母
+            int id = ch - 'a';                                      // 字母对应 id
+            if (!cur->next[id]) cur->next[id] = new node(fork);     // 如果分支为空指针，建立新节点
+            cur = cur->next[id];                                    // 指针指向下一个节点
+        }
+        cur->isword = true;                                         // 设置标记位
     }
-    // isw[rt] = true;      // 标志该单词末位字母的尾结点，在查询整个单词时用到
-}
-bool find(char *s) {
-    int len = strlen(s), rt = 0;  // 从根结点开始找
-    rep(i, 0, len) {
-        int id = s[i] - 'a';
-        if(!trie[rt][id]) return false; 
-        rt = trie[rt][id];      // 为查询下个字母做准备，往下走 
-    }
-    return true;            // 作为前缀出现
-    // return isw[rt];      // 是否作为完整的单词出现过
-    // return sum[rt];      // 前缀出现的次数 
-}
+};
