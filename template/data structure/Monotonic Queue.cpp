26db3ad0c5344d89753c117a175566ba01594b01
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-07-27 00:26:32 +0800

    update monotonic queue

--- template/data structure/Monotonic Queue.cpp
+++ template/data structure/Monotonic Queue.cpp
@@ -1,11 +1,12 @@
-deque < pair<ll, int> > Q;
+/* ----- 单调队列 ----- */
 
-void Push(ll val, int id) {
-    while (!Q.empty() && Q.back().fi < val) Q.pop_back();
-    Q.pb(mp(val, id));
-}
-
-ll Front(int id) {
-    while (!Q.empty() && id - Q.front().se > K) Q.pop_front(); 
-    return Q.front().fi;
-}
+template <class T, class Comp=less<T>>                              // T: 元素类型； Comp: 关系类仿函数
+struct Monotonic_Queue {                                            // 默认队首最小的单调队列
+    deque <T> Q;                                                    // 容器 deque
+    inline T& front() { return Q.front(); }                         // 返回队首的引用
+    inline void push(T x) {                                         // 队尾加入元素 x
+        while (!Q.empty() && Comp()(x, Q.back())) Q.pop_back();     // 默认：队尾大于等于 x 的元素出队
+        Q.push_back(x);                                             // 元素 x 入队
+    }
+    inline void pop() { Q.pop_front(); }                            // 队首出队
+};
