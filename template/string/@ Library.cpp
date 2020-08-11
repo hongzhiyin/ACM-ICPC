a4b4e01ad7c0c52f147e905f022ed4fc8e7e6ce8
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-08-11 19:16:27 +0800

    update

--- template/string/@ Library.cpp
+++ template/string/@ Library.cpp
@@ -1,5 +1,5 @@
 void trim(string &str) {
-    string blanks("\f\v\r\t\n ");
-    str.erase(0,str.find_first_not_of(blanks));
-    str.erase(str.find_last_not_of(blanks) + 1);
+    string space("\f\v\r\t\n ");
+    str.erase(0,str.find_first_not_of(space));
+    str.erase(str.find_last_not_of(space) + 1);
 }
