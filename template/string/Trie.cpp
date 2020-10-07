23ea6918db7f000939f7ab8e47921446260d15ab
hongzhiyin <34067497+hongzhiyin@users.noreply.github.com>
2020-10-07 11:21:26 +0800

    Update Trie.cpp

--- template/string/Trie.cpp
+++ template/string/Trie.cpp
@@ -10,18 +10,18 @@ struct node {                                 // 字典树节点
 };
 
 struct Trie {
-    typedef node *Iter;                                          // 用于访问节点的指针
-    Iter root;                                                   // 根节点指针
-    int fork;                                                    // 节点分支数
-    Trie(int fork) : fork(fork) { root = new node(fork); }       // 初始化，建立根节点
+    typedef node *Iter;                                              // 用于访问节点的指针
+    Iter root;                                                       // 根节点指针
+    int fork;                                                        // 节点分支数
+    Trie(){} Trie(int fork) : fork(fork) { root = new node(fork); }  // 初始化，建立根节点
 
-    void insert(string &s) {                                     // 插入单词
-        node *cur = root;                                        // 临时指针
-        for (auto ch : s) {                                      // 遍历每个字母
-            int id = ch - 'a';                                   // 字母对应 id
-            if (!cur->next[id]) cur->next[id] = new node(fork);  // 如果分支为空指针，建立新节点
-            cur = cur->next[id];                                 // 指针指向下一个节点
+    void insert(string &s) {                                         // 插入单词
+        node *cur = root;                                            // 临时指针
+        for (auto ch : s) {                                          // 遍历每个字母
+            int id = ch - 'a';                                       // 字母对应 id
+            if (!cur->next[id]) cur->next[id] = new node(fork);      // 如果分支为空指针，建立新节点
+            cur = cur->next[id];                                     // 指针指向下一个节点
         }
-        cur->isword = true;                                      // 设置标记位
+        cur->isword = true;                                          // 设置标记位
     }
 };
